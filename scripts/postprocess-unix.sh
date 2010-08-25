0eddcfed00845bed33af96ea35f4882c34195c07
rvt <rvt@c6892bbe-2a20-4548-a94f-41916fb2bb95>
2010-08-25 02:51:43 +0000

    Make sure that all translations are getting compiled and loaded in the resource folder.
    git-svn-id: https://librecad.svn.sourceforge.net/svnroot/librecad/trunk@18 c6892bbe-2a20-4548-a94f-41916fb2bb95

--- scripts/postprocess-unix.sh
+++ scripts/postprocess-unix.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-RESOURCEDIR=unix/resources
+RESOURCEDIR="`pwd`/unix/resources"
+TSDIR="`pwd`/ts"
 
 # Postprocess for unix
 mkdir -p $RESOURCEDIR/fonts
 mkdir -p $RESOURCEDIR/patterns
 cp support/patterns/*.dxf $RESOURCEDIR/patterns
 cp support/fonts/*.cxf $RESOURCEDIR/fonts
+
+
+# Generate translations
+lrelease caduntu.pro
+
+mkdir -p $RESOURCEDIR/qm
+ 
+# Go into translations directory
+cd "$TSDIR"
+for dir in actions cmd lib main ui
+do 
+    cd $dir
+    for tf in *.qm
+    do
+	cp $tf $RESOURCEDIR/qm/$tf
+    done
+    
+    cd "$TSDIR" 
+done
