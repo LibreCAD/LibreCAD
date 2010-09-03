ba7cfcdd5f5e883d56749adc80426b13254d7d25
rvt <rvt@c6892bbe-2a20-4548-a94f-41916fb2bb95>
2010-09-03 03:09:30 +0000

    Added icons to menu's Draw->Point and Draw->Line
    git-svn-id: https://librecad.svn.sourceforge.net/svnroot/librecad/trunk@43 c6892bbe-2a20-4548-a94f-41916fb2bb95

--- src/actions/rs_actiondrawlinefree.cpp
+++ src/actions/rs_actiondrawlinefree.cpp
@@ -45,9 +45,9 @@ RS_ActionDrawLineFree::~RS_ActionDrawLineFree() {
 }
 
 QAction* RS_ActionDrawLineFree::createGUIAction(RS2::ActionType /*type*/, QObject* /*parent*/) {
-/* RVT_PORT    QAction* action = new QAction(tr("Line: Freehand"), tr("&Freehand Line"),
-                                  QKeySequence(), NULL); */
+	// tr("&Freehand Line"),
     QAction* action = new QAction(tr("Line: Freehand"), NULL);
+	action->setIcon(QIcon(":/extui/linesfree.png"));
     action->setStatusTip(tr("Draw freehand lines"));
     return action;
 }
