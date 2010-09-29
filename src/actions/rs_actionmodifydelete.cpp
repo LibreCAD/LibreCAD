6e4f8079be2d9c1c9faa073a7581bf5f3d6f07d2
rvt <rvt@c6892bbe-2a20-4548-a94f-41916fb2bb95>
2010-09-29 02:42:40 +0000

    Changed behavior of the commit key. Now goes back to the main toolbar after pressing the delete key.
    git-svn-id: https://librecad.svn.sourceforge.net/svnroot/librecad/trunk@64 c6892bbe-2a20-4548-a94f-41916fb2bb95

--- src/actions/rs_actionmodifydelete.cpp
+++ src/actions/rs_actionmodifydelete.cpp
@@ -43,7 +43,7 @@ QAction* RS_ActionModifyDelete::createGUIAction(RS2::ActionType /*type*/, QObjec
     QAction* action = new QAction(tr("&Delete"),  NULL);
 	action->setIcon(QIcon(":/extui/modifydelete.png"));
     action->setStatusTip(tr("Delete Entities"));
-	action->setShortcut(QKeySequence::Delete);
+	//action->setShortcut(QKeySequence::Delete);
     return action;
 }
 
