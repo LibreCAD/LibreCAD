6d741191b971ea8eb447105a778c3eb11056b605
rvt <rvt@c6892bbe-2a20-4548-a94f-41916fb2bb95>
2010-12-27 01:38:45 +0000

    Removed XPM and replaced with PNG files under res/main Changed png file to show LibreCAD
    git-svn-id: https://librecad.svn.sourceforge.net/svnroot/librecad/trunk@106 c6892bbe-2a20-4548-a94f-41916fb2bb95

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -7,7 +7,7 @@
 **
 **
 ** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by 
+** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
 **
@@ -15,12 +15,12 @@
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 **
-** This copyright notice MUST APPEAR in all copies of the script!  
+** This copyright notice MUST APPEAR in all copies of the script!
 **
 **********************************************************************/
 
@@ -31,11 +31,11 @@
 
 #include <qtimer.h>
 
-# include <qsplashscreen.h>
+# include <QSplashScreen>
 //Added by qt3to4:
 #include <q3mimefactory.h>
 #include <QPixmap>
-  QSplashScreen *splash = 0;
+QSplashScreen *splash;
 
 #ifdef RS_SCRIPTING
 #include <qsproject.h>
@@ -67,7 +67,7 @@ extern void QINITIMAGES_LIBRECAD();
 #endif
 
 #ifdef QC_BUILTIN_STYLE
-	extern void applyBuiltinStyle();
+        extern void applyBuiltinStyle();
 #endif
 
 
@@ -75,17 +75,16 @@ extern void QINITIMAGES_LIBRECAD();
 /**
  * Main. Creates Application window.
  *
- * @todo Switch back to xpm instead of png. 
  * Cleaning up #defines.
  */
 int main(int argc, char** argv) {
-	
+
     RS_DEBUG->setLevel(RS_Debug::D_WARNING);
     // RS_DEBUG->setLevel(RS_Debug::D_DEBUGGING);
-	RS_DEBUG->print("param 0: %s", argv[0]);
+        RS_DEBUG->print("param 0: %s", argv[0]);
 
-	// avoid . / , confusion on German environments
-	setlocale(LC_ALL, "C");
+        // avoid . / , confusion on German environments
+        setlocale(LC_ALL, "C");
 
         QCoreApplication::setApplicationName(XSTR(QC_APPNAME));
         QCoreApplication::setApplicationVersion(XSTR(QC_VERSION));
@@ -93,207 +92,195 @@ int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
 
-	// for image mime resources from png files
-	//  TODO: kinda dirty to call that explicitly
-	QINITIMAGES_LIBRECAD();
+        // for image mime resources from png files
+        //  TODO: kinda dirty to call that explicitly
+        QINITIMAGES_LIBRECAD();
 #ifdef RS_SCRIPTING
 //	qInitImages_librecad();
 #endif
 
-	for (int i=0; i<app.argc(); i++) {
-		if (QString("--debug") == app.argv()[i]) {
-    		RS_DEBUG->setLevel(RS_Debug::D_DEBUGGING);
-		}
-	}
-	
-	QFileInfo prgInfo( QFile::decodeName(argv[0]) );
-	QString prgDir(prgInfo.dirPath(true));
+        for (int i=0; i<app.argc(); i++) {
+                if (QString("--debug") == app.argv()[i]) {
+                RS_DEBUG->setLevel(RS_Debug::D_DEBUGGING);
+                }
+        }
+
+        QFileInfo prgInfo( QFile::decodeName(argv[0]) );
+        QString prgDir(prgInfo.dirPath(true));
     RS_SETTINGS->init(XSTR(QC_COMPANYKEY), XSTR(QC_APPKEY));
     RS_SYSTEM->init(XSTR(QC_APPNAME), XSTR(QC_VERSION), XSTR(QC_APPDIR), prgDir);
-	
-	RS_FILEIO->registerFilter(new RS_FilterCXF());
-	RS_FILEIO->registerFilter(new RS_FilterDXF());
-	RS_FILEIO->registerFilter(new RS_FilterDXF1());
-#ifdef RS_OPT_CAM 
-	RS_FILEIO->registerFilter(new RS_FilterCAM());
+
+        RS_FILEIO->registerFilter(new RS_FilterCXF());
+        RS_FILEIO->registerFilter(new RS_FilterDXF());
+        RS_FILEIO->registerFilter(new RS_FilterDXF1());
+#ifdef RS_OPT_CAM
+        RS_FILEIO->registerFilter(new RS_FilterCAM());
 #endif
 
-	// parse command line arguments that might not need a launched program:
-	QStringList fileList = handleArgs(argc, argv);
+        // parse command line arguments that might not need a launched program:
+        QStringList fileList = handleArgs(argc, argv);
 
-	RS_String lang;
-	RS_String langCmd;
-	RS_String unit;
-	
-	RS_SETTINGS->beginGroup("/Defaults");
+        RS_String lang;
+        RS_String langCmd;
+        RS_String unit;
+
+        RS_SETTINGS->beginGroup("/Defaults");
 #ifndef QC_PREDEFINED_UNIT
-	unit = RS_SETTINGS->readEntry("/Unit", "Invalid");
+        unit = RS_SETTINGS->readEntry("/Unit", "Invalid");
 #else
-	unit = RS_SETTINGS->readEntry("/Unit", QC_PREDEFINED_UNIT);
+        unit = RS_SETTINGS->readEntry("/Unit", QC_PREDEFINED_UNIT);
 #endif
     RS_SETTINGS->endGroup();
 
-	// show initial config dialog:
-	if (unit=="Invalid") {
-		RS_DEBUG->print("main: show initial config dialog..");
-		QG_DlgInitial di(NULL);
-		di.setText("<font size=\"+1\"><b>Welcome to " XSTR(QC_APPNAME) "</b></font>"
-		"<br>"
-		"Please choose the unit you want to use for new drawings and your "
-		"preferred language.<br>"
-		"You can changes these settings later in the "
-		"Options Dialog of " XSTR(QC_APPNAME) ".");
+        // show initial config dialog:
+        if (unit=="Invalid") {
+                RS_DEBUG->print("main: show initial config dialog..");
+                QG_DlgInitial di(NULL);
+                di.setText("<font size=\"+1\"><b>Welcome to " XSTR(QC_APPNAME) "</b></font>"
+                "<br>"
+                "Please choose the unit you want to use for new drawings and your "
+                "preferred language.<br>"
+                "You can changes these settings later in the "
+                "Options Dialog of " XSTR(QC_APPNAME) ".");
                 QPixmap pxm(":/main/intro_librecad.png");
-		di.setPixmap(pxm);
-		if (di.exec()) {
-			RS_SETTINGS->beginGroup("/Defaults");
-			unit = RS_SETTINGS->readEntry("/Unit", "None");
-    		RS_SETTINGS->endGroup();
-		}
-		RS_DEBUG->print("main: show initial config dialog: OK");
-	}
+                di.setPixmap(pxm);
+                if (di.exec()) {
+                        RS_SETTINGS->beginGroup("/Defaults");
+                        unit = RS_SETTINGS->readEntry("/Unit", "None");
+                RS_SETTINGS->endGroup();
+                }
+                RS_DEBUG->print("main: show initial config dialog: OK");
+        }
 
 #ifdef QSPLASHSCREEN_H
-	RS_DEBUG->print("main: splashscreen..");
+        RS_DEBUG->print("main: splashscreen..");
 
-# ifdef QC_CUSTOM_SPLASH
-	QPixmap* pixmap = new QPixmap(qPixmapFromMimeSource(QC_CUSTOM_SPLASH)); 
-# else
         QPixmap* pixmap = new QPixmap(":/main/splash_librecad.png");
 # endif
 
-/*
-	splash = new QSplashScreen(*pixmap);
-	splash->show();
-	splash->message(app.tr("Loading.."), 
-		Qt::AlignRight|Qt::AlignBottom, QC_SPLASH_TXTCOL);
-	RS_DEBUG->print("main: splashscreen: OK");
-*/
-#endif
-
-	RS_DEBUG->print("main: init fontlist..");
+        RS_DEBUG->print("main: init fontlist..");
     RS_FONTLIST->init();
-	RS_DEBUG->print("main: init fontlist: OK");
-	
-	RS_DEBUG->print("main: init patternlist..");
+        RS_DEBUG->print("main: init fontlist: OK");
+
+        RS_DEBUG->print("main: init patternlist..");
     RS_PATTERNLIST->init();
-	RS_DEBUG->print("main: init patternlist: OK");
+        RS_DEBUG->print("main: init patternlist: OK");
 
-	RS_DEBUG->print("main: init scriptlist..");
+        RS_DEBUG->print("main: init scriptlist..");
     RS_SCRIPTLIST->init();
-	RS_DEBUG->print("main: init scriptlist: OK");
+        RS_DEBUG->print("main: init scriptlist: OK");
 
-	RS_DEBUG->print("main: loading translation..");
-	RS_SETTINGS->beginGroup("/Appearance");
+        RS_DEBUG->print("main: loading translation..");
+        RS_SETTINGS->beginGroup("/Appearance");
 #ifdef QC_PREDEFINED_LOCALE
-		lang = RS_SETTINGS->readEntry("/Language", "");
-		if (lang.isEmpty()) {
-			lang=QC_PREDEFINED_LOCALE;
-			RS_SETTINGS->writeEntry("/Language", lang);
-		}
-		langCmd = RS_SETTINGS->readEntry("/LanguageCmd", "");
-		if (langCmd.isEmpty()) {
-			langCmd=QC_PREDEFINED_LOCALE;
-			RS_SETTINGS->writeEntry("/LanguageCmd", langCmd);
-		}
+                lang = RS_SETTINGS->readEntry("/Language", "");
+                if (lang.isEmpty()) {
+                        lang=QC_PREDEFINED_LOCALE;
+                        RS_SETTINGS->writeEntry("/Language", lang);
+                }
+                langCmd = RS_SETTINGS->readEntry("/LanguageCmd", "");
+                if (langCmd.isEmpty()) {
+                        langCmd=QC_PREDEFINED_LOCALE;
+                        RS_SETTINGS->writeEntry("/LanguageCmd", langCmd);
+                }
 #else
     lang = RS_SETTINGS->readEntry("/Language", "en");
     langCmd = RS_SETTINGS->readEntry("/LanguageCmd", "en");
 #endif
-	RS_SETTINGS->endGroup();
-	
-	RS_SYSTEM->loadTranslation(lang, langCmd);
-	RS_DEBUG->print("main: loading translation: OK");
+        RS_SETTINGS->endGroup();
+
+        RS_SYSTEM->loadTranslation(lang, langCmd);
+        RS_DEBUG->print("main: loading translation: OK");
 
 #ifdef QSPLASHSCREEN_H
-	splash = new QSplashScreen(*pixmap);
-	splash->show();
-	splash->message(QObject::tr("Loading.."), 
-		Qt::AlignRight|Qt::AlignBottom, QC_SPLASH_TXTCOL);
-	RS_DEBUG->print("main: splashscreen: OK");
+        splash = new QSplashScreen(*pixmap);
+        splash->show();
+        splash->message(QObject::tr("Loading.."),
+                Qt::AlignRight|Qt::AlignBottom, QC_SPLASH_TXTCOL);
+        RS_DEBUG->print("main: splashscreen: OK");
 #endif
 
     //QApplication::setStyle(new QWindowsStyle());
     //QApplication::setStyle(new QPlatinumStyle());
 
 #ifdef QC_BUILTIN_STYLE //js:
-	RS_DEBUG->print("main: applying built in style..");
-	applyBuiltinStyle();
+        RS_DEBUG->print("main: applying built in style..");
+        applyBuiltinStyle();
 #endif
 
-	RS_DEBUG->print("main: creating main window..");
+        RS_DEBUG->print("main: creating main window..");
     QC_ApplicationWindow * appWin = new QC_ApplicationWindow();
-	RS_DEBUG->print("main: setting caption");
+        RS_DEBUG->print("main: setting caption");
     appWin->setCaption(XSTR(QC_APPNAME));
-	RS_DEBUG->print("main: show main window");
+        RS_DEBUG->print("main: show main window");
     appWin->show();
-	RS_DEBUG->print("main: set focus");
-	appWin->setFocus();
-	RS_DEBUG->print("main: creating main window: OK");
+        RS_DEBUG->print("main: set focus");
+        appWin->setFocus();
+        RS_DEBUG->print("main: creating main window: OK");
 
 #ifdef QSPLASHSCREEN_H
-	if (splash) {
-		RS_DEBUG->print("main: updating splash..");
-		splash->message(QObject::tr("Loading Library Paths.."), 
-			Qt::AlignRight|Qt::AlignBottom, QC_SPLASH_TXTCOL);
-		RS_DEBUG->print("main: processing events");
-		qApp->processEvents();
-		RS_DEBUG->print("main: updating splash: OK");
-	}
+        if (splash) {
+                RS_DEBUG->print("main: updating splash..");
+                splash->message(QObject::tr("Loading Library Paths.."),
+                        Qt::AlignRight|Qt::AlignBottom, QC_SPLASH_TXTCOL);
+                RS_DEBUG->print("main: processing events");
+                qApp->processEvents();
+                RS_DEBUG->print("main: updating splash: OK");
+        }
 #endif
 
-	// loading files:
-	RS_DEBUG->print("main: loading files..");
-	bool files_loaded = false;
-	for (QStringList::Iterator it = fileList.begin(); it != fileList.end(); 
-		++it ) {
-		
+        // loading files:
+        RS_DEBUG->print("main: loading files..");
+        bool files_loaded = false;
+        for (QStringList::Iterator it = fileList.begin(); it != fileList.end();
+                ++it ) {
+
 #ifdef QSPLASHSCREEN_H
-			if (splash) {
-				splash->message(QObject::tr("Loading File %1..")
-					.arg(QDir::convertSeparators(*it)), 
-				Qt::AlignRight|Qt::AlignBottom, QC_SPLASH_TXTCOL);
-				qApp->processEvents();
-			}
+                        if (splash) {
+                                splash->message(QObject::tr("Loading File %1..")
+                                        .arg(QDir::convertSeparators(*it)),
+                                Qt::AlignRight|Qt::AlignBottom, QC_SPLASH_TXTCOL);
+                                qApp->processEvents();
+                        }
 #endif
-			appWin->slotFileOpen(*it, RS2::FormatUnknown);
-			files_loaded = true;
-	}
-	RS_DEBUG->print("main: loading files: OK");
-			
+                        appWin->slotFileOpen(*it, RS2::FormatUnknown);
+                        files_loaded = true;
+        }
+        RS_DEBUG->print("main: loading files: OK");
+
 #ifdef QSPLASHSCREEN_H
 # ifndef QC_DELAYED_SPLASH_SCREEN
-	if (splash) {
-		splash->finish(appWin);
-		delete splash;
-		splash = 0;
-	}
+        if (splash) {
+                splash->finish(appWin);
+                delete splash;
+                splash = 0;
+        }
 # endif
-	delete pixmap;
+        delete pixmap;
 #endif
 
     //app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
 
-	// renew: avoid . / , confusion on German environments
-	//setlocale(LC_NUMERIC, "C");
-	setlocale(LC_ALL, "C");
-	
-	RS_DEBUG->print("main: app.exec()");
+        // renew: avoid . / , confusion on German environments
+        //setlocale(LC_NUMERIC, "C");
+        setlocale(LC_ALL, "C");
 
-	if (!files_loaded) {
-		appWin->slotFileNew();
-	}
+        RS_DEBUG->print("main: app.exec()");
 
-	appWin->slotRunStartScript();
-	
-	int r = app.exec();
+        if (!files_loaded) {
+                appWin->slotFileNew();
+        }
 
-	RS_DEBUG->print("main: Temporary disabled  delete appWin");
-	// delete appWin;
+        appWin->slotRunStartScript();
 
-	RS_DEBUG->print("main: finished");
+        int r = app.exec();
 
-	return r;
+        RS_DEBUG->print("main: Temporary disabled  delete appWin");
+        // delete appWin;
+
+        RS_DEBUG->print("main: finished");
+
+        return r;
 }
 
 
@@ -304,80 +291,80 @@ int main(int argc, char** argv) {
  * @return list of files to load on startup.
  */
 QStringList handleArgs(int argc, char** argv) {
-	RS_DEBUG->print("main: handling args..");
-	QStringList ret;
+        RS_DEBUG->print("main: handling args..");
+        QStringList ret;
 
-	bool doexit = false;
-	QString machine;
-	QString input;
-	QString output;
-	
-	for (int i=1; i<argc; i++) {
-		if (QString(argv[i]).startsWith("-")==false) {
-			QString fname = QDir::convertSeparators( 
-				QFileInfo(QFile::decodeName(argv[i])).absFilePath() );
-			ret.append(fname);
-		}
-		else if (QString(argv[i])=="--exit") {
-			doexit = true;
-		}
+        bool doexit = false;
+        QString machine;
+        QString input;
+        QString output;
+
+        for (int i=1; i<argc; i++) {
+                if (QString(argv[i]).startsWith("-")==false) {
+                        QString fname = QDir::convertSeparators(
+                                QFileInfo(QFile::decodeName(argv[i])).absFilePath() );
+                        ret.append(fname);
+                }
+                else if (QString(argv[i])=="--exit") {
+                        doexit = true;
+                }
 #ifdef RS_CAM
-		else if (QString(argv[i])=="--convert") {
-			++i;
-			if (i<argc) {
-				machine = QString(argv[i]);
-			}
-			else {
-        		RS_DEBUG->print(RS_Debug::D_WARNING,
-					"No machine configuration given after --convert. " 
-				    "Aborting..");
-				exit(1);
-			}
-			++i;
-			if (i<argc) {
-				input = QString(argv[i]);
-			}
-			else {
-        		RS_DEBUG->print(RS_Debug::D_WARNING,
-					"No input given after --convert. " 
-				    "Aborting..");
-				exit(1);
-			}
-			++i;
-			if (i<argc) {
-				output = QString(argv[i]);
-			}
-			else {
-        		RS_DEBUG->print(RS_Debug::D_WARNING,
-					"No output given after --convert. " 
-				    "Aborting..");
-				exit(1);
-			}
-		}
+                else if (QString(argv[i])=="--convert") {
+                        ++i;
+                        if (i<argc) {
+                                machine = QString(argv[i]);
+                        }
+                        else {
+                        RS_DEBUG->print(RS_Debug::D_WARNING,
+                                        "No machine configuration given after --convert. "
+                                    "Aborting..");
+                                exit(1);
+                        }
+                        ++i;
+                        if (i<argc) {
+                                input = QString(argv[i]);
+                        }
+                        else {
+                        RS_DEBUG->print(RS_Debug::D_WARNING,
+                                        "No input given after --convert. "
+                                    "Aborting..");
+                                exit(1);
+                        }
+                        ++i;
+                        if (i<argc) {
+                                output = QString(argv[i]);
+                        }
+                        else {
+                        RS_DEBUG->print(RS_Debug::D_WARNING,
+                                        "No output given after --convert. "
+                                    "Aborting..");
+                                exit(1);
+                        }
+                }
 #endif
-	}
+        }
 
 #ifdef RS_CAM
-	// auto cam convert
-	if (machine.isEmpty()==false && input.isEmpty()==false && 
-		output.isEmpty()==false) {
+        // auto cam convert
+        if (machine.isEmpty()==false && input.isEmpty()==false &&
+                output.isEmpty()==false) {
 
-		RS_FilterCAM fc;
-		RS_Graphic gr;
-		RS_FILEIO->fileImport(gr, input, RS2::FormatUnknown);
-		RS_CamDialog dlg(gr, NULL);
-		dlg.activateMachineGenerator(machine);
-		dlg.externalOK();
-		fc.sort(gr);
-		fc.fileExport(gr, output, RS2::FormatCAM);
-	}
+                RS_FilterCAM fc;
+                RS_Graphic gr;
+                RS_FILEIO->fileImport(gr, input, RS2::FormatUnknown);
+                RS_CamDialog dlg(gr, NULL);
+                dlg.activateMachineGenerator(machine);
+                dlg.externalOK();
+                fc.sort(gr);
+                fc.fileExport(gr, output, RS2::FormatCAM);
+        }
 #endif
 
-	if (doexit) {
-		exit(0);
-	}
-	
-	RS_DEBUG->print("main: handling args: OK");
-	return ret;
+        if (doexit) {
+                exit(0);
+        }
+
+        RS_DEBUG->print("main: handling args: OK");
+        return ret;
 }
 
