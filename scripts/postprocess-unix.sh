3219dfd164585e986111d6782296bc49880aafc1
Ries van Twisk <ries@librecad.org>
2011-04-20 21:41:18 -0500

    Added support for showing helpfiles. It's not yet context sensitive, but at least it shows help and it's browsable.

--- scripts/postprocess-unix.sh
+++ scripts/postprocess-unix.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+THISDIR="`pwd`"
 RESOURCEDIR="`pwd`/unix/resources"
 TSDIR="`pwd`/ts"
+DOCDIR="`pwd`/support/doc"
+
+# Generate Help Files
+cd "$DOCDIR"
+qcollectiongenerator LibreCADdoc.qhcp
+
+cd "$THISDIR"
 
 # Postprocess for unix
 mkdir -p $RESOURCEDIR/fonts
 mkdir -p $RESOURCEDIR/patterns
+mkdir -p $RESOURCEDIR/doc
 cp support/patterns/*.dxf $RESOURCEDIR/patterns
 cp support/fonts/*.cxf $RESOURCEDIR/fonts
+cp support/doc/*.qhc $RESOURCEDIR/doc
+cp support/doc/*.qch $RESOURCEDIR/doc
 find support/library -type d -not -path "*.svn*"  | sed s/support// | xargs -IFILES  mkdir $RESOURCEDIR/FILES
 find support/library -type f -iname *.dxf -not -path "*.svn*"  | sed s/support// | xargs -IFILES  cp support/FILES $RESOURCEDIR/FILES
 
@@ -27,3 +38,4 @@ do
     
     cd "$TSDIR" 
 done
+
