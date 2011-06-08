46711fce1926137a6750f8f58b306c9a4c5ddc29
R. van Twisk <librecad@rvt.dds.nl>
2011-06-08 01:05:05 -0500

    Added locale C for LC_NUMERIC to make sure numeric input on command works as expected, . is always dicimal seperator and , between points

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -237,6 +237,9 @@ int main(int argc, char** argv) {
         }
         RS_DEBUG->print("main: loading files: OK");
 
+        // Set LC_NUMERIC so that enetring numeric values uses . as teh decimal seperator
+        setlocale(LC_NUMERIC, "C");
+
 #ifdef QSPLASHSCREEN_H
 # ifndef QC_DELAYED_SPLASH_SCREEN
         if (splash) {
