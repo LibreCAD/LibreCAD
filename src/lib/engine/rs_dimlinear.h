cfef38e602a8d64949d5225e571f9e133a477f98
Rallaz <rallazz@gmail.com>
2011-06-15 16:46:21 +0200

    changed Q3PtrList to QList in RS_EntityContainer

--- src/lib/engine/rs_dimlinear.h
+++ src/lib/engine/rs_dimlinear.h
@@ -93,7 +93,7 @@ public:
 
     virtual RS_Entity* clone() {
         RS_DimLinear* d = new RS_DimLinear(*this);
-		d->entities.setAutoDelete(entities.autoDelete());
+        d->setOwner(isOwner());
         d->initId();
         d->detach();
         return d;
