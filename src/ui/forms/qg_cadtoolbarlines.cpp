df771fd79298c8774fad70bbf1a856b2d794b872
Rallaz <rallazz@gmail.com>
2011-07-10 13:31:28 +0200

    Qt4port of src/ui/forms/qg_cadtoolbar*

--- src/ui/forms/qg_cadtoolbarlines.cpp
+++ src/ui/forms/qg_cadtoolbarlines.cpp
@@ -25,15 +25,14 @@
 **********************************************************************/
 #include "qg_cadtoolbarlines.h"
 
-#include <qvariant.h>
 #include "qg_cadtoolbar.h"
-#include "qg_cadtoolbarlines.ui.h"
+
 /*
  *  Constructs a QG_CadToolBarLines as a child of 'parent', with the
  *  name 'name' and widget flags set to 'f'.
  */
-QG_CadToolBarLines::QG_CadToolBarLines(QWidget* parent, const char* name, Qt::WindowFlags fl)
-    : QWidget(parent, name, fl)
+QG_CadToolBarLines::QG_CadToolBarLines(QWidget* parent, Qt::WindowFlags fl)
+    : QWidget(parent, fl)
 {
     setupUi(this);
 
@@ -57,3 +56,132 @@ void QG_CadToolBarLines::languageChange()
     retranslateUi(this);
 }
 
+void QG_CadToolBarLines::init() {
+    actionHandler = NULL;
+    cadToolBar = NULL;
+}
+
+/*void QG_CadToolBarLines::mousePressEvent(QMouseEvent* e) {
+    if (e->button()==RightButton && cadToolBar!=NULL) {
+        cadToolBar->back();
+        e->accept();
+    }
+}*/
+
+
+
+void QG_CadToolBarLines::contextMenuEvent(QContextMenuEvent *e) {
+    e->accept();
+}
+
+void QG_CadToolBarLines::setCadToolBar(QG_CadToolBar* tb) {
+    cadToolBar = tb;
+    if (tb!=NULL) {
+        actionHandler = tb->getActionHandler();
+    } else {
+        RS_DEBUG->print(RS_Debug::D_ERROR,
+                        "QG_CadToolBarLines::setCadToolBar(): No valid toolbar set.");
+    }
+}
+
+void QG_CadToolBarLines::drawLine() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLine();
+    }
+}
+
+void QG_CadToolBarLines::drawLineAngle() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineAngle();
+    }
+}
+
+void QG_CadToolBarLines::drawLineHorizontal() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineHorizontal();
+    }
+}
+
+void QG_CadToolBarLines::drawLineHorVert() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineHorVert();
+    }
+}
+
+void QG_CadToolBarLines::drawLineVertical() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineVertical();
+    }
+}
+
+void QG_CadToolBarLines::drawLineParallel() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineParallel();
+    }
+}
+
+void QG_CadToolBarLines::drawLineParallelThrough() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineParallelThrough();
+    }
+}
+
+void QG_CadToolBarLines::drawLineRectangle() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineRectangle();
+    }
+}
+
+void QG_CadToolBarLines::drawLineBisector() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineBisector();
+    }
+}
+
+void QG_CadToolBarLines::drawLineTangent1() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineTangent1();
+    }
+}
+
+void QG_CadToolBarLines::drawLineTangent2() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineTangent2();
+    }
+}
+
+void QG_CadToolBarLines::drawLineOrthogonal() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineOrthogonal();
+    }
+}
+
+void QG_CadToolBarLines::drawLineRelAngle() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineRelAngle();
+    }
+}
+
+void QG_CadToolBarLines::drawLineFree() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLineFree();
+    }
+}
+
+void QG_CadToolBarLines::drawLinePolygon() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLinePolygon();
+    }
+}
+
+void QG_CadToolBarLines::drawLinePolygon2() {
+    if (cadToolBar!=NULL && actionHandler!=NULL) {
+        actionHandler->slotDrawLinePolygon2();
+    }
+}
+
+void QG_CadToolBarLines::back() {
+    if (cadToolBar!=NULL) {
+        cadToolBar->back();
+    }
+}
