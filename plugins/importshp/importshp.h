9a0672171aab0f053a534d3be775b86cbee8bec4
Rallaz <rallazz@gmail.com>
2011-07-15 19:20:10 +0200

    Start of plugin for import shp files, can import points whitout attributes

---
+++ plugins/importshp/importshp.h
@@ -0,0 +1,88 @@
+/*****************************************************************************/
+/*  importshp.h - shape file importer                                        */
+/*                                                                           */
+/*  Copyright (C) 2011 Rallaz, rallazz@gmail.com                             */
+/*                                                                           */
+/*  This library is free software, licensed under the terms of the GNU       */
+/*  General Public License as published by the Free Software Foundation,     */
+/*  either version 2 of the License, or (at your option) any later version.  */
+/*  You should have received a copy of the GNU General Public License        */
+/*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    */
+/*****************************************************************************/
+
+#ifndef IMPORTSHP_H
+#define IMPORTSHP_H
+
+#include <QLabel>
+#include <QGroupBox>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QDialog>
+#include "qc_plugininterface.h"
+#include "document_interface.h"
+
+class pointData;
+
+class ImportShp : public QObject, QC_PluginInterface
+{
+    Q_OBJECT
+     Q_INTERFACES(QC_PluginInterface)
+
+ public:
+     virtual PluginCapabilities getCapabilities() const;
+     virtual QString name() const;
+     virtual void execComm(Document_Interface *doc,
+                                        QWidget *parent, QString cmd);
+};
+
+/*namespace DPT {
+    enum txtposition {N, S, E, O, NE, SE, SO, NO};
+}*/
+
+class dibSHP : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit dibSHP(QWidget *parent = 0);
+    ~dibSHP();
+//    void SetupUI(QWidget *parent);
+    void procesFile(Document_Interface *doc);
+
+public slots:
+    void getFile();
+    void checkAccept();
+    void updateFile();
+
+private:
+    void readSettings();
+    void writeSettings();
+
+private:
+    QLineEdit *fileedit;
+    QComboBox *layerdata;
+    QComboBox *colordata;
+    QComboBox *ltypedata;
+    QComboBox *lwidthdata;
+    QComboBox *pointdata;
+    QGroupBox *pointbox;
+    QLabel *formattype;
+
+//    QList<pointData*> dataList;
+
+    Document_Interface *currDoc;
+
+};
+
+/***********/
+class pointData
+{
+public:
+    QString number;
+    QString x;
+    QString y;
+    QString z;
+    QString code;
+};
+/***********/
+#endif // IMPORTSHP_H
---
+++ plugins/importshp/importshp.pro
@@ -0,0 +1,50 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-03-22T19:33:11
+#
+#-------------------------------------------------
+
+QT       += gui
+TEMPLATE = lib
+CONFIG += plugin
+VERSION = 1.0.0
+PLUGIN_NAME=importshp
+
+SOURCES += importshp.cpp \
+           shapelib/shpopen.c \
+           shapelib/safileio.c \
+           shapelib/dbfopen.c
+HEADERS += importshp.h \
+           shapelib/shapefil.h
+
+win32 {
+    Debug {
+        DLLDESTDIR = ../../debug/resources/plugins
+        TARGET = $$PLUGIN_NAME
+
+    } else {
+        DLLDESTDIR = ../../release/resources/plugins
+        TARGET = $$PLUGIN_NAME
+    }
+}
+unix {
+    macx { 
+	TARGET = ../../LibreCAD.app/Contents/Resources/plugins/$$PLUGIN_NAME
+    }
+    else { 
+	TARGET = ../../unix/resources/plugins/$$PLUGIN_NAME
+    }
+}
+
+INCLUDEPATH    += ../../src/plugins \
+                  shapelib-1.3.0b2
+
+# Store intermedia stuff somewhere else
+OBJECTS_DIR = ../intermediate/obj
+MOC_DIR = ../intermediate/moc
+RCC_DIR = ../intermediate/rcc
+TS_DIR = ../intermediate/ts
+UI_DIR = ../intermediate/ui
+UI_HERADERS_DIR = ../intermediate/ui
+UI_SOURCES_DIR = ../intermediate/ui
+
