dd4aa026ae0f51cacae9d2a7f8fc568235338aaf
Dongxu Li <dongxuli2011@gmail.com>
2011-09-22 13:56:00 -0400

    Updated CadToolBarArcs to new snap system

--- src/ui/forms/qg_cadtoolbararcs.cpp
+++ src/ui/forms/qg_cadtoolbararcs.cpp
@@ -7,7 +7,7 @@
 **
 **
 ** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software 
+** GNU General Public License version 2 as published by the Free Software
 ** Foundation and appearing in the file gpl-2.0.txt included in the
 ** packaging of this file.
 **
@@ -15,12 +15,12 @@
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 **
-** This copyright notice MUST APPEAR in all copies of the script!  
+** This copyright notice MUST APPEAR in all copies of the script!
 **
 **********************************************************************/
 #include "qg_cadtoolbararcs.h"
@@ -115,9 +115,4 @@ void QG_CadToolBarArcs::back() {
     }
 }
 
-void QG_CadToolBarArcs::on_bArcTangential_clicked()
-{
-    if (cadToolBar!=NULL && actionHandler!=NULL) {
-        actionHandler->slotDrawArcTangential();
-    }
-}
+//EOF
