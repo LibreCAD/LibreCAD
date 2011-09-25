809552e69316dfce09e00e251e80cd78c76da175
Dongxu Li <dongxuli2011@gmail.com>
2011-09-24 21:25:03 -0400

    detecting file modified, bug# 3366392

--- src/ui/qg_recentfiles.cpp
+++ src/ui/qg_recentfiles.cpp
@@ -7,7 +7,7 @@
 **
 **
 ** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software 
+** GNU General Public License version 2 as published by the Free Software
 ** Foundation and appearing in the file gpl-2.0.txt included in the
 ** packaging of this file.
 **
@@ -15,12 +15,12 @@
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 **
-** This copyright notice MUST APPEAR in all copies of the script!  
+** This copyright notice MUST APPEAR in all copies of the script!
 **
 **********************************************************************/
 
@@ -43,12 +43,12 @@ QG_RecentFiles::QG_RecentFiles(int number) {
 QG_RecentFiles::~QG_RecentFiles() {}
 
 /**
- * Adds a file to the list of recently loaded files if 
+ * Adds a file to the list of recently loaded files if
  * it's not already in the list.
  */
 void QG_RecentFiles::add(const QString& filename) {
-	RS_DEBUG->print("QG_RecentFiles::add");
-	
+        RS_DEBUG->print("QG_RecentFiles::add");
+
     // is the file already in the list?
     if (files.indexOf(filename) >= 0) {
         return;
@@ -65,7 +65,7 @@ void QG_RecentFiles::add(const QString& filename) {
     //for (int i=0; i<(int)files.count(); ++i) {
     //	printf("recent file[%d]: %s\n", i, files[i].latin1());
     //}
-	RS_DEBUG->print("QG_RecentFiles::add: OK");
+        RS_DEBUG->print("QG_RecentFiles::add: OK");
 }
 
 
