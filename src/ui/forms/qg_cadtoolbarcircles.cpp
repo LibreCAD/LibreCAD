51add700b252d1da8c8c3fa9b7ce0e2fbcdca46b
Dongxu Li <dongxuli2011@gmail.com>
2011-09-25 23:36:18 -0400

    Start default action based on checked button

--- src/ui/forms/qg_cadtoolbarcircles.cpp
+++ src/ui/forms/qg_cadtoolbarcircles.cpp
@@ -7,7 +7,7 @@
 **
 **
 ** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software 
+** GNU General Public License version 2 as published by the Free Software
 ** Foundation and appearing in the file gpl-2.0.txt included in the
 ** packaging of this file.
 **
@@ -15,12 +15,12 @@
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 **
-** This copyright notice MUST APPEAR in all copies of the script!  
+** This copyright notice MUST APPEAR in all copies of the script!
 **
 **********************************************************************/
 #include "qg_cadtoolbarcircles.h"
@@ -117,3 +117,24 @@ void QG_CadToolBarCircles::back() {
         cadToolBar->back();
     }
 }
+//restore action from checked button
+void QG_CadToolBarCircles::restoreAction()
+{
+    if(actionHandler==NULL) return;
+    if ( bCircle ->isChecked() ) {
+        actionHandler->slotDrawCircle();
+    }
+    if ( bCircleCR ->isChecked() ) {
+        actionHandler->slotDrawCircleCR();
+    }
+    if ( bCircle2P ->isChecked() ) {
+        actionHandler->slotDrawCircle2P();
+    }
+    if ( bCircle3P ->isChecked() ) {
+        actionHandler->slotDrawCircle3P();
+    }
+    if ( bCircleParallel ->isChecked() ) {
+        actionHandler->slotDrawCircleParallel();
+    }
+}
+//EOF
