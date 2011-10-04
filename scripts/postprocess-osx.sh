55a573b084ea854f9943828e66c6223f4b320a79
R. van Twisk <rvt@localjahia.com>
2011-10-03 19:43:48 -0500

    Change postprocessor for OSX so it will include a SQL plugin

--- scripts/postprocess-osx.sh
+++ scripts/postprocess-osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 THISDIR="`pwd`"
-RESOURCEDIR="`pwd`/LibreCAD.app/Contents/Resources"
+RESOURCEDIR="`pwd`/LibreCAD.app/Contents"
 TSDIR="`pwd`/ts"
 DOCDIR="`pwd`/support/doc"
 
@@ -12,25 +12,27 @@ qcollectiongenerator LibreCADdoc.qhcp
 cd "$THISDIR"
 
 # Postprocess for osx
-mkdir -p $RESOURCEDIR/fonts
-mkdir -p $RESOURCEDIR/patterns
-mkdir -p $RESOURCEDIR/doc
-cp support/patterns/*.dxf $RESOURCEDIR/patterns
-cp support/fonts/*.cxf $RESOURCEDIR/fonts
-cp support/fonts/*.lff $RESOURCEDIR/fonts
-cp support/doc/*.qhc $RESOURCEDIR/doc
-cp support/doc/*.qch $RESOURCEDIR/doc
-find support/library -type d -not -path "*.svn*"  | sed s/support// | xargs -IFILES  mkdir $RESOURCEDIR/FILES 
-find support/library -type f -iname *.dxf -not -path "*.svn*"  | sed s/support// | xargs -IFILES  cp support/FILES $RESOURCEDIR/FILES 
+mkdir -p $RESOURCEDIR/Resources/fonts
+mkdir -p $RESOURCEDIR/Resources/patterns
+mkdir -p $RESOURCEDIR/Resources/doc
+mkdir -p $RESOURCEDIR/PlugIns
+cp support/patterns/*.dxf $RESOURCEDIR/Resources/patterns
+cp support/fonts/*.cxf $RESOURCEDIR/Resources/fonts
+cp support/doc/*.qhc $RESOURCEDIR/Resources/doc
+cp support/doc/*.qch $RESOURCEDIR/Resources/doc
+find support/library -type d -not -path "*.svn*"  | sed s/support// | xargs -IFILES  mkdir $RESOURCEDIR/Resources/FILES 
+find support/library -type f -iname *.dxf -not -path "*.svn*"  | sed s/support// | xargs -IFILES  cp support/FILES $RESOURCEDIR/Resources/FILES 
+
+cp -r /Developer/Applications/Qt/plugins/sqldrivers $RESOURCEDIR/PlugIns/sqldrivers
 
 # Generate translations
 lrelease librecad.pro
-mkdir -p $RESOURCEDIR/qm
+mkdir -p $RESOURCEDIR/Resources/qm
  
 # Go into translations directory
 cd "$TSDIR"
 for tf in *.qm
 do
-		cp $tf $RESOURCEDIR/qm/$tf
+	cp $tf $RESOURCEDIR/Resources/qm/$tf
 done
 
