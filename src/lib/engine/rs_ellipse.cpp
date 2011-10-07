ce9f0fdb1601570ab2982e6ba4253419d91b48ba
Dongxu Li <dongxuli2011@gmail.com>
2011-10-07 17:31:01 -0400

    minor file cleanup for rs_ellipse

--- src/lib/engine/rs_ellipse.cpp
+++ src/lib/engine/rs_ellipse.cpp
@@ -993,14 +993,14 @@ void RS_Ellipse::draw(RS_Painter* painter, RS_GraphicView* view, double /*patter
         return;
     }
     double styleFactor = getStyleFactor(view);
-    if (styleFactor<=0.0||getAngleLength()<=RS_TOLERANCE_ANGLE) {
-        std::cout<<"2\n";
+    if (styleFactor<=0.0){
         painter->drawEllipse(cp,
                              ra,
                              rb,
                              mAngle,
                              getAngle1(), getAngle2(),
                              isReversed());
+        RS_DEBUG->print(RS_Debug::D_WARNING,"RS_Ellipse::draw(): negative Style Factor, do not use pattern for drawing");
         return;
     }
 
@@ -1023,7 +1023,7 @@ void RS_Ellipse::draw(RS_Painter* painter, RS_GraphicView* view, double /*patter
     painter->setPen(pen);
     double* ds = new double[pat->num];
     int i(0);
-    double amin(5.);
+    double amin(3.);
     double patternLength=0.;
     while( i<pat->num){
         ds[i]=fabs(pat->pattern[i] * styleFactor);//pattern length
@@ -1032,10 +1032,10 @@ void RS_Ellipse::draw(RS_Painter* painter, RS_GraphicView* view, double /*patter
         i++;
     }
     if(amin>RS_TOLERANCE){
-        amin=5./amin;
+        amin=3./amin;
         patternLength *= amin;
-        if (patternLength > 100.) {
-            amin *= 100./patternLength;
+        if (patternLength > 50.) {
+            amin *= 50./patternLength;
 //            patternLength=100.;
         }
         for(i=0;i<pat->num;i++){
