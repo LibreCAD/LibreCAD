598c93ad5cd616fc967da5e0688d9b380c82f21b
Dongxu Li <dongxuli2011@gmail.com>
2011-11-15 14:42:33 -0500

    layer freeze renamed to hide: bug#3437188

--- src/actions/rs_actionlayersfreezeall.cpp
+++ src/actions/rs_actionlayersfreezeall.cpp
@@ -7,7 +7,7 @@
 **
 **
 ** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software 
+** GNU General Public License version 2 as published by the Free Software
 ** Foundation and appearing in the file gpl-2.0.txt included in the
 ** packaging of this file.
 **
@@ -15,12 +15,12 @@
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 **
-** This copyright notice MUST APPEAR in all copies of the script!  
+** This copyright notice MUST APPEAR in all copies of the script!
 **
 **********************************************************************/
 
@@ -42,19 +42,19 @@ RS_ActionLayersFreezeAll::RS_ActionLayersFreezeAll(bool freeze,
 
 QAction* RS_ActionLayersFreezeAll::createGUIAction(RS2::ActionType type, QObject* /*parent*/) {
     QAction* action=NULL;
-	
-	if (type==RS2::ActionLayersFreezeAll) {
-		// tr("Freeze all")
-		action = new QAction(tr("&Freeze all"),  NULL);
-    	//action->zetStatusTip(tr("Freeze all layers"));
-		action->setIcon(QIcon(":/ui/hiddenblock.png"));
-	}
-	else if (type==RS2::ActionLayersDefreezeAll) {
-		// tr("Defreeze all")
-        action = new QAction(tr("&Defreeze all"),   NULL);
+
+        if (type==RS2::ActionLayersFreezeAll) {
+                // tr("Freeze all")
+                action = new QAction(tr("&Hide all"),  NULL);
+        //action->zetStatusTip(tr("Freeze all layers"));
+                action->setIcon(QIcon(":/ui/hiddenblock.png"));
+        }
+        else if (type==RS2::ActionLayersDefreezeAll) {
+                // tr("Defreeze all")
+        action = new QAction(tr("&Unhide all"),   NULL);
         //action->zetStatusTip(tr("Defreeze all layers"));
-		action->setIcon(QIcon(":/ui/visibleblock.png"));
-	}
+                action->setIcon(QIcon(":/ui/visibleblock.png"));
+        }
     return action;
 }
 
