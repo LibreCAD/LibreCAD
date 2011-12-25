21e21ed4ec58684eec101e02dfd5e823b8b90624
Dongxu Li <dongxuli2011@gmail.com>
2011-12-25 17:47:38 -0500

    painter: use more QVector methods

--- src/lib/gui/rs_painter.cpp
+++ src/lib/gui/rs_painter.cpp
@@ -7,7 +7,7 @@
 **
 **
 ** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software 
+** GNU General Public License version 2 as published by the Free Software
 ** Foundation and appearing in the file gpl-2.0.txt included in the
 ** packaging of this file.
 **
@@ -15,12 +15,12 @@
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 **
-** This copyright notice MUST APPEAR in all copies of the script!  
+** This copyright notice MUST APPEAR in all copies of the script!
 **
 **********************************************************************/
 
@@ -33,78 +33,48 @@ void RS_Painter::createArc(QPolygon& pa,
                              double a1, double a2,
                              bool reversed) {
 
-	if (radius<1.0e-6) {
+    if (radius<1.0e-6) {
         RS_DEBUG->print(RS_Debug::D_WARNING,
-			"RS_Painter::createArc: invalid radius: %f", radius);
-		return;
-	}
-
-    int   cix;            // Next point on circle
-    int   ciy;            //
-    double aStep;         // Angle Step (rad)
-    double a;             // Current Angle (rad)
-
-    if(fabs(2.0/radius)<=1.0) {
-        aStep=asin(2.0/radius);
-    } else {
-        aStep=1.0;
+            "RS_Painter::createArc: invalid radius: %f", radius);
+        return;
     }
 
-    aStep=aStep/2.0;
+    double aStart=a1;
+    double aEnd=a2;
+    if(a2<a1) a2 += 2.*M_PI;
+    double aStep=fabs(2.0/radius);         // Angle Step (rad)
+    if(aStep>=0.5) aStep=0.5;
+    if(reversed) {
+        std::swap(aStart,aEnd);
+        aStep *= -1;
+    }
+    double a;             // Current Angle (rad)
+
+//    aStep=aStep/2.0;
     //if (aStep<0.05) {
     //    aStep = 0.05;
     //}
-	
-	// less than a pixel long lines:
-	//if (radius*aStep<1.0) {
-	//	aStep = 
-	//}
+
+    // less than a pixel long lines:
+    //if (radius*aStep<1.0) {
+    //	aStep =
+    //}
 
     //QPointArray pa;
-    int i=0;
-    pa.resize(i+1);
-    pa.setPoint(i++, toScreenX(cp.x+cos(a1)*radius),
-                toScreenY(cp.y-sin(a1)*radius));
-    //moveTo(toScreenX(cp.x+cos(a1)*radius),
-    //       toScreenY(cp.y-sin(a1)*radius));
-    if(!reversed) {
-        // Arc Counterclockwise:
-        if(a1>a2-1.0e-10) {
-            a2+=2*M_PI;
-        }
-        for(a=a1+aStep; a<=a2; a+=aStep) {
-            cix = toScreenX(cp.x+cos(a)*radius);
-            ciy = toScreenY(cp.y-sin(a)*radius);
-            //lineTo(cix, ciy);
-            pa.resize(i+1);
-            pa.setPoint(i++, cix, ciy);
-        }
-    } else {
-        // Arc Clockwise:
-        if(a1<a2+1.0e-10) {
-            a2-=2*M_PI;
-        }
-        for(a=a1-aStep; a>=a2; a-=aStep) {
-            cix = toScreenX(cp.x+cos(a)*radius);
-            ciy = toScreenY(cp.y-sin(a)*radius);
-            //lineTo(cix, ciy);
-            pa.resize(i+1);
-            pa.setPoint(i++, cix, ciy);
-        }
+    pa.clear();
+    pa<<QPoint(toScreenX(cp.x+cos(aStart)*radius), toScreenY(cp.y-sin(aStart)*radius));
+
+    for(a=aStart+aStep; fabs(a-aStart)<fabs(aEnd-aStart); a+=aStep) {
+        pa<<QPoint(toScreenX(cp.x+cos(a)*radius), toScreenY(cp.y-sin(a)*radius));
     }
-    //lineTo(toScreenX(cp.x+cos(a2)*radius),
-    //       toScreenY(cp.y-sin(a2)*radius));
-    pa.resize(i+1);
-    pa.setPoint(i++,
-                toScreenX(cp.x+cos(a2)*radius),
-                toScreenY(cp.y-sin(a2)*radius));
-    //drawPolyline(pa);
+
+    pa<<QPoint(toScreenX(cp.x+cos(aEnd)*radius), toScreenY(cp.y-sin(aEnd)*radius));
 }
 
 
 
 void RS_Painter::drawRect(const RS_Vector& p1, const RS_Vector& p2) {
-	drawLine(RS_Vector(p1.x, p1.y), RS_Vector(p2.x, p1.y));
+    drawLine(RS_Vector(p1.x, p1.y), RS_Vector(p2.x, p1.y));
     drawLine(RS_Vector(p2.x, p1.y), RS_Vector(p2.x, p2.y));
     drawLine(RS_Vector(p2.x, p2.y), RS_Vector(p1.x, p2.y));
     drawLine(RS_Vector(p1.x, p2.y), RS_Vector(p1.x, p1.y));
@@ -113,10 +83,10 @@ void RS_Painter::drawRect(const RS_Vector& p1, const RS_Vector& p2) {
 
 
 void RS_Painter::drawHandle(const RS_Vector& p, const RS_Color& c, int size) {
-	if (size<0) {
-		size = 2;
-	}
-	fillRect((int)(p.x-size), (int)(p.y-size), 2*size, 2*size, c);
+    if (size<0) {
+        size = 2;
+    }
+    fillRect((int)(p.x-size), (int)(p.y-size), 2*size, 2*size, c);
 }
 
 
