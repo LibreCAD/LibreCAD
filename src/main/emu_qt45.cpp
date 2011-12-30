09c4dbcf0643ebce5bddf54678dd40fcfde3eb00
tin-pot <tin-pot@gmx.net>
2011-12-30 14:18:28 +0100

    emu_qt45_QInputDialog_getInt().

--- src/main/emu_qt45.cpp
+++ src/main/emu_qt45.cpp
@@ -5,21 +5,19 @@
 #if QT_VERSION < 0x040500
 
 #include <QWidget>
+#include <QInputDialog>
+
 #include "emu_qt45.h"
 
 int emu_qt45_QInputDialog_getInt(QWidget * parent, 
-                                 const QString & title, 
-                                 const QString & label, 
-                                 int value,
-                                 int min,
-                                 int max,
-                                 int step,
-                                 bool * ok,
-                                 Qt::WindowFlags flags)
+                                 const QString & title, const QString & label, 
+                                 int value, int min, int max, int step,
+                                 bool * ok, Qt::WindowFlags flags)
 {
-    if (ok != NULL)
-        *ok = false;
-    return INT_MIN;
+    return QInputDialog::getInteger(parent, 
+                                    title, label, 
+                                    value, min, max, step, 
+                                    ok, flags);
 }
 
 #endif // QT_VERSION 0x040500
