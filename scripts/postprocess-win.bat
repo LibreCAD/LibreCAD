b85d01775d81318142458328b749b672c854716c
unknown <Ries van Twisk@RiesvanTwisk-PC.(none)>
2012-02-07 21:31:13 -0500

    Small changes to Windows scripts

--- scripts/postprocess-win.bat
+++ scripts/postprocess-win.bat
@@ -8,36 +8,36 @@ set TSDIR=%PWD%\ts
 set DOCDIR=%PWD%\support\doc
 
 REM Generate Help Files
-cd %DOCDIR%
+cd "%DOCDIR%"
 qcollectiongenerator LibreCADdoc.qhcp
 
-cd %PWD%
+cd "%PWD%"
 
 REM Postprocess for windows
 echo " Copying fonts and patterns"
-mkdir %RESOURCEDIR%\fonts
-mkdir %RESOURCEDIR%\patterns
-mkdir %RESOURCEDIR%\library
-mkdir %RESOURCEDIR%\doc
-mkdir %RESOURCEDIR%\library\misc
-mkdir %RESOURCEDIR%\library\templates
-copy support\patterns\*.dxf %RESOURCEDIR%\patterns
-copy support\fonts\*.cxf %RESOURCEDIR%\fonts
-copy support\fonts\*.lff %RESOURCEDIR%\fonts
-copy support\doc\*.qhc %RESOURCEDIR%\doc
-copy support\doc\*.qch %RESOURCEDIR%\doc
-copy support\library\misc\*.dxf %RESOURCEDIR%\library\misc
-copy support\library\templates\*.dxf %RESOURCEDIR%\library\templates
+mkdir "%RESOURCEDIR%\fonts"
+mkdir "%RESOURCEDIR%\patterns"
+mkdir "%RESOURCEDIR%\library"
+mkdir "%RESOURCEDIR%\doc"
+mkdir "%RESOURCEDIR%\library\misc"
+mkdir "%RESOURCEDIR%\library\templates"
+copy "support\patterns\*.dxf" "%RESOURCEDIR%\patterns"
+copy "support\fonts\*.cxf" "%RESOURCEDIR%\fonts"
+copy "support\fonts\*.lff" "%RESOURCEDIR%\fonts"
+copy "support\doc\*.qhc" "%RESOURCEDIR%\doc"
+copy "support\doc\*.qch" "%RESOURCEDIR%\doc"
+copy "support\library\misc\*.dxf" "%RESOURCEDIR%\library\misc"
+copy "support\library\templates\*.dxf" "%RESOURCEDIR%\library\templates"
 
 
 REM Generate translations
 echo "Generating Translations"
 lrelease librecad.pro
-mkdir %RESOURCEDIR%\qm 
+mkdir "%RESOURCEDIR%\qm"
 
-cd %TSDIR%
+cd "%TSDIR%"
 for /f %%F in ('dir /b *.qm') do (
-	copy %%F %RESOURCEDIR%\qm\%%F
+        copy "%%F" "%RESOURCEDIR%\qm\%%F"
 )
 
-cd %PWD%
+cd "%PWD%"
