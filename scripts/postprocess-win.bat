9f3621c84b3e7c6e41251763a3637f76ec471cc0
R. van Twisk <librecad@rvt.dds.nl>
2012-02-11 13:11:53 -0500

    OSX removed copy of old cxf fonts Windows Fixed builds Added simple documentation

--- scripts/postprocess-win.bat
+++ scripts/postprocess-win.bat
@@ -3,45 +3,41 @@
 cd > PWD
 set /p PWD= < PWD
 
-set RESOURCEDIR=%PWD%\windows\resources
+set RESOURCEDIR=%PWD%\release\resources
 set TSDIR=%PWD%\ts
 set DOCDIR=%PWD%\support\doc
 
 REM Generate Help Files
-cd %DOCDIR%
-%QTDIR%\bin\qcollectiongenerator.exe LibreCADdoc.qhcp
+cd "%DOCDIR%"
+qcollectiongenerator LibreCADdoc.qhcp
 
-cd %PWD%
+cd "%PWD%"
 
 REM Postprocess for windows
 echo " Copying fonts and patterns"
-mkdir %RESOURCEDIR%\fonts
-mkdir %RESOURCEDIR%\patterns
-mkdir %RESOURCEDIR%\library
-mkdir %RESOURCEDIR%\doc
-mkdir %RESOURCEDIR%\library\misc
-mkdir %RESOURCEDIR%\library\templates
-copy support\patterns\*.dxf %RESOURCEDIR%\patterns
-copy support\fonts\*.cxf %RESOURCEDIR%\fonts
-copy support\fonts\*.lff %RESOURCEDIR%\fonts
-copy support\doc\*.qhc %RESOURCEDIR%\doc
-copy support\doc\*.qch %RESOURCEDIR%\doc
-copy support\library\misc\*.dxf %RESOURCEDIR%\library\misc
-copy support\library\templates\*.dxf %RESOURCEDIR%\library\templates
+mkdir "%RESOURCEDIR%\fonts"
+mkdir "%RESOURCEDIR%\patterns"
+mkdir "%RESOURCEDIR%\library"
+mkdir "%RESOURCEDIR%\doc"
+mkdir "%RESOURCEDIR%\library\misc"
+mkdir "%RESOURCEDIR%\library\templates"
+copy "support\patterns\*.dxf" "%RESOURCEDIR%\patterns"
+copy "support\fonts\*.cxf" "%RESOURCEDIR%\fonts"
+copy "support\fonts\*.lff" "%RESOURCEDIR%\fonts"
+copy "support\doc\*.qhc" "%RESOURCEDIR%\doc"
+copy "support\doc\*.qch" "%RESOURCEDIR%\doc"
+copy "support\library\misc\*.dxf" "%RESOURCEDIR%\library\misc"
+copy "support\library\templates\*.dxf" "%RESOURCEDIR%\library\templates"
 
 
 REM Generate translations
 echo "Generating Translations"
-cd %TSDIR%
-for /f %%F in ('dir /b *.ts') do (
-  %QTDIR%\bin\lrelease.exe %%F
-)
+lrelease librecad.pro
+mkdir "%RESOURCEDIR%\qm"
 
-mkdir %RESOURCEDIR%\qm 
-
-cd %TSDIR%
+cd "%TSDIR%"
 for /f %%F in ('dir /b *.qm') do (
-	copy %%F %RESOURCEDIR%\qm\%%F
+        copy "%%F" "%RESOURCEDIR%\qm\%%F"
 )
 
-cd %PWD%
+cd "%PWD%"
