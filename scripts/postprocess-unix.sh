573dc9c239377f8f770c75dcb0cb0457d085c99b
Dongxu Li <dongxuli2011@gmail.com>
2012-03-03 22:37:17 -0500

    postprocess-unix: added quotes as suggested by iku

--- scripts/postprocess-unix.sh
+++ scripts/postprocess-unix.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 THISDIR="`pwd`"
-RESOURCEDIR="`pwd`/unix/resources"
-TSDIR="`pwd`/ts"
-DOCDIR="`pwd`/support/doc"
+RESOURCEDIR="${THISDIR}/unix/resources"
+TSDIR="${THISDIR}/ts"
+DOCDIR="${THISDIR}/support/doc"
 
 # Generate Help Files
-cd "$DOCDIR"
+cd "{$DOCDIR}"
 qcollectiongenerator LibreCADdoc.qhcp
 
-cd "$THISDIR"
+cd "${THISDIR}"
 
 # Postprocess for unix
-mkdir -p $RESOURCEDIR/fonts
-mkdir -p $RESOURCEDIR/patterns
-mkdir -p $RESOURCEDIR/doc
-cp support/patterns/*.dxf $RESOURCEDIR/patterns
-#cp support/fonts/*.cxf $RESOURCEDIR/fonts
-cp support/fonts/*.lff* $RESOURCEDIR/fonts
-#cp support/doc/*.qhc $RESOURCEDIR/doc
-#cp support/doc/*.qch $RESOURCEDIR/doc
-find support/library -type d -not -path "*.svn*"  | sed s/support// | xargs -IFILES  mkdir $RESOURCEDIR/FILES
-find support/library -type f -iname *.dxf -not -path "*.svn*"  | sed s/support// | xargs -IFILES  cp support/FILES $RESOURCEDIR/FILES
+mkdir -p "${RESOURCEDIR}"/fonts
+mkdir -p "${RESOURCEDIR}"/patterns
+mkdir -p "${RESOURCEDIR}"/doc
+cp support/patterns/*.dxf "${RESOURCEDIR}"/patterns
+#cp support/fonts/*.cxf "${RESOURCEDIR}"/fonts
+cp support/fonts/*.lff* "${RESOURCEDIR}"/fonts
+#cp support/doc/*.qhc "${RESOURCEDIR}"/doc
+#cp support/doc/*.qch "${RESOURCEDIR}"/doc
+find support/library -type d | sed s/support// | xargs -IFILES  mkdir "${RESOURCEDIR}"/FILES
+find support/library -type f -iname *.dxf | sed s/support// | xargs -IFILES  cp support/FILES "${RESOURCEDIR}"/FILES
 
 # Generate translations
 lrelease librecad.pro
-mkdir -p $RESOURCEDIR/qm
+mkdir -p "${RESOURCEDIR}"/qm
  
 # Go into translations directory
-cd "$TSDIR"
+cd "${TSDIR}"
 for tf in *.qm
 do
-		cp $tf $RESOURCEDIR/qm/$tf
+		cp "${tf}" "${RESOURCEDIR}/qm/${tf}"
 done
 
