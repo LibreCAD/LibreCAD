f9f33776e56327a89f05ca741fbdfbe5c0ab1c55
Dongxu Li <dongxuli2011@gmail.com>
2012-03-31 05:39:14 -0400

    filedlg: removed space in default filename

--- librecad/src/ui/qg_filedialog.cpp
+++ librecad/src/ui/qg_filedialog.cpp
@@ -7,7 +7,7 @@
 **
 **
 ** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software 
+** GNU General Public License version 2 as published by the Free Software
 ** Foundation and appearing in the file gpl-2.0.txt included in the
 ** packaging of this file.
 **
@@ -15,12 +15,12 @@
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 **
-** This copyright notice MUST APPEAR in all copies of the script!  
+** This copyright notice MUST APPEAR in all copies of the script!
 **
 **********************************************************************/
 
@@ -206,10 +206,10 @@ QString QG_FileDialog::getSaveFile(RS2::FormatType* type){
     if(QFile::exists( defDir + fn + getExtension( ftype ) ))
     {
         int fileCount = 1;
-        while(QFile::exists( defDir + fn + QString(" %1").arg(fileCount) +
+        while(QFile::exists( defDir + fn + QString("%1").arg(fileCount) +
                              getExtension(ftype)) )
             ++fileCount;
-        fn += QString(" %1").arg(fileCount);
+        fn += QString("%1").arg(fileCount);
     }
 
     // initialize dialog properties
