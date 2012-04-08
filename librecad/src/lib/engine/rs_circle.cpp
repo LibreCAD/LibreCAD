47754425b58674eebeb0bf2947829af6d62592a1
Dongxu Li <dongxuli2011@gmail.com>
2012-04-08 13:35:01 -0400

    Appollonius: fixed an error in equation solver

--- librecad/src/lib/engine/rs_circle.cpp
+++ librecad/src/lib/engine/rs_circle.cpp
@@ -328,8 +328,8 @@ bool RS_Circle::testTan3(const QVector<RS_AtomicEntity*>& circles)
     for(auto it=circles.begin();it!=itEnd;it++){
 //        std::cout<<"with Center = ( "<<(*it)->getCenter().x<<" , "<<(*it)->getCenter().y<<" ), r= "<<(*it)->getRadius()<<std::endl;
         const double dist=fabs((data.center - (*it)->getCenter()).magnitude());
-        if(  fabs(dist -  fabs( data.radius - (*it)->getRadius()))>1.e2*RS_TOLERANCE
-             && fabs(dist - data.radius - (*it)->getRadius())>1.e2*RS_TOLERANCE ) return false;
+        if(  fabs(dist -  fabs( data.radius - (*it)->getRadius()))>RS_TOLERANCE
+             && fabs(dist - data.radius - (*it)->getRadius())>RS_TOLERANCE ) return false;
 //        std::cout<<"accepted"<<std::endl;
     }
     return true;
@@ -341,10 +341,10 @@ with Cx the center of the common tangent circle, Rx the radius. Ci and Ri are th
 **/
 QList<RS_Circle> RS_Circle::solveAppolloniusSingle(const QList<RS_Circle>& circles)
 {
-          std::cout<<__FILE__<<" : "<<__FUNCTION__<<" : line "<<__LINE__<<std::endl;
-          for(int i=0;i<circles.size();i++){
-std::cout<<"i="<<i<<"\t center="<<circles[i].getCenter()<<"\tr="<<circles[i].getRadius()<<std::endl;
-          }
+//          std::cout<<__FILE__<<" : "<<__FUNCTION__<<" : line "<<__LINE__<<std::endl;
+//          for(int i=0;i<circles.size();i++){
+//std::cout<<"i="<<i<<"\t center="<<circles[i].getCenter()<<"\tr="<<circles[i].getRadius()<<std::endl;
+//          }
     QList<RS_Circle> ret;
 
     QList<RS_Vector> centers;
@@ -367,30 +367,31 @@ std::cout<<"i="<<i<<"\t center="<<circles[i].getCenter()<<"\tr="<<circles[i].get
     // r^0 term
     mat[0][2]=0.5*(centers[2].squared()-centers[0].squared()+radii[0]*radii[0]-radii[2]*radii[2]);
     mat[1][2]=0.5*(centers[2].squared()-centers[1].squared()+radii[1]*radii[1]-radii[2]*radii[2]);
-    std::cout<<__FILE__<<" : "<<__FUNCTION__<<" : line "<<__LINE__<<std::endl;
-    for(unsigned short i=0;i<=1;i++){
-        std::cout<<"eqs P:"<<i<<" : "<<mat[i][0]<<"*x + "<<mat[i][1]<<"*y = "<<mat[i][2]<<std::endl;
-    }
-    QVector<QVector<double> > sm(2,QVector<double>(2,0.));
-    if(RS_Math::linearSolver(mat,sm[0])==false){
+//    std::cout<<__FILE__<<" : "<<__FUNCTION__<<" : line "<<__LINE__<<std::endl;
+//    for(unsigned short i=0;i<=1;i++){
+//        std::cout<<"eqs P:"<<i<<" : "<<mat[i][0]<<"*x + "<<mat[i][1]<<"*y = "<<mat[i][2]<<std::endl;
+//    }
+//    QVector<QVector<double> > sm(2,QVector<double>(2,0.));
+    QVector<double> sm(2,0.);
+    if(RS_Math::linearSolver(mat,sm)==false){
         return ret;
     }
 
-    RS_Vector vp(sm[0][0],sm[0][1]);
-      std::cout<<__FILE__<<" : "<<__FUNCTION__<<" : line "<<__LINE__<<std::endl;
-      std::cout<<"vp="<<vp<<std::endl;
+    RS_Vector vp(sm[0],sm[1]);
+//      std::cout<<__FILE__<<" : "<<__FUNCTION__<<" : line "<<__LINE__<<std::endl;
+//      std::cout<<"vp="<<vp<<std::endl;
 
     // r term
     mat[0][2]= radii[0]-radii[2];
     mat[1][2]= radii[1]-radii[2];
-    for(unsigned short i=0;i<=1;i++){
-        std::cout<<"eqs Q:"<<i<<" : "<<mat[i][0]<<"*x + "<<mat[i][1]<<"*y = "<<mat[i][2]<<std::endl;
-    }
-    if(RS_Math::linearSolver(mat,sm[1])==false){
+//    for(unsigned short i=0;i<=1;i++){
+//        std::cout<<"eqs Q:"<<i<<" : "<<mat[i][0]<<"*x + "<<mat[i][1]<<"*y = "<<mat[i][2]<<std::endl;
+//    }
+    if(RS_Math::linearSolver(mat,sm)==false){
         return ret;
     }
-    RS_Vector vq(sm[1][0],sm[1][1]);
-      std::cout<<"vq="<<vq<<std::endl;
+    RS_Vector vq(sm[0],sm[1]);
+//      std::cout<<"vq="<<vq<<std::endl;
     //form quadratic equation for r
     RS_Vector dcp=vp-centers[0];
     double a=vq.squared()-1.;
@@ -405,8 +406,8 @@ std::cout<<"i="<<i<<"\t center="<<circles[i].getCenter()<<"\tr="<<circles[i].get
 //        if(vr.at(i)<RS_TOLERANCE) continue;
         ret<<RS_Circle(NULL,RS_CircleData(vp+vq*vr.at(i),vr.at(i)));
     }
-    std::cout<<__FILE__<<" : "<<__FUNCTION__<<" : line "<<__LINE__<<std::endl;
-    std::cout<<"Found "<<ret.size()<<" solutions"<<std::endl;
+//    std::cout<<__FILE__<<" : "<<__FUNCTION__<<" : line "<<__LINE__<<std::endl;
+//    std::cout<<"Found "<<ret.size()<<" solutions"<<std::endl;
 
     return ret;
 }
