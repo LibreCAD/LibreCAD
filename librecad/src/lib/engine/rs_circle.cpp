f17aebe08c27b16b31fdf57a03d17768ff2565fb
Dongxu Li <dongxuli2011@gmail.com>
2012-06-03 20:08:32 -0400

    painter: fixed pen for circles

--- librecad/src/lib/engine/rs_circle.cpp
+++ librecad/src/lib/engine/rs_circle.cpp
@@ -657,7 +657,8 @@ bool RS_Circle::isVisibleInWindow(RS_GraphicView* view) const
 /** draw circle as a 2 pi arc */
 void RS_Circle::draw(RS_Painter* painter, RS_GraphicView* view, double& patternOffset) {
     RS_Arc arc(getParent(), RS_ArcData(getCenter(),getRadius(),0.,2.*M_PI, false));
-            arc.draw(painter,view,patternOffset);
+    arc.setPen(getPen());
+    arc.draw(painter,view,patternOffset);
 }
 
 
