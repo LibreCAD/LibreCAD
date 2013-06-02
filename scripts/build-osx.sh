32cb804a6f78fa7c8fbf56a0b0158a7a44321dbc
Dongxu Li <dli@tamaggo.com>
2013-06-02 16:53:37 -0400

    building script for os/x

---
+++ scripts/build-osx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+# this script builds LibreCAD in OS/X
+# The dependency required to be able to build LibreCAD:
+# qt, boost, muparser
+
+# the LibreCAD source folder 
+cd ../
+qmake librecad.pro -r -spec mkspecs/macports
+make -j4
+macdeployqt LibreCAD.app -verbose=2 -dmg
