ed44ad081a6ab46412c0e4f87b3fa6076065dbae
Armin Stebich <home@mail.lordofbikes.de>
2013-06-03 23:46:45 +0200

    adding scripts/custom-windows.bat for path issues with Qt and NSIS

--- scripts/build-windows.bat
+++ scripts/build-windows.bat
@@ -1,6 +1,10 @@
 
 set Qt_DIR=C:\Qt\Qt5.0.2\5.0.2
-set PATH=%Qt_DIR%\mingw47_32\bin;%Qt_DIR%\..\Tools\MinGW\bin;C:\Program Files (x86)\NSIS\Bin;%PATH%
+set NSIS_DIR=C:\Program Files (x86)\NSIS
+
+if exist custom-windows.bat call custom-windows.bat
+
+set PATH=%Qt_DIR%\mingw47_32\bin;%Qt_DIR%\..\Tools\MinGW\bin;%NSIS_DIR%\Bin;%PATH%
 
 cd ../..
 mkdir LibreCAD-Release
