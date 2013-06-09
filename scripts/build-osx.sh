712deaf2335e40cc0861c59e71195142484df8d2
Dongxu Li <dongxuli2011@gmail.com>
2013-06-08 20:59:16 -0400

    updated os/x building script

--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -5,7 +5,11 @@
 # qt, boost, muparser
 
 # the LibreCAD source folder 
-cd ../
+
+SCRIPT="$(realpath "$0")"
+SCRIPTPATH="$(dirname "${SCRIPT}")"
+
+cd "${SCRIPTPATH}"/..
 qmake librecad.pro -r -spec mkspec/macports
 make -j4
 rm -f LibreCAD.dmg
