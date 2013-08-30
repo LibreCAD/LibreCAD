1386ddba6f3c9421649dd373e138d0eaacd02a21
Dongxu Li <dli@tamaggo.com>
2013-08-30 01:27:33 -0400

    updated windows building scripts for Qt-5.1.1

--- scripts/set-windows-env.bat
+++ scripts/set-windows-env.bat
@@ -5,11 +5,12 @@ if "%NSIS_DIR%"=="" goto SetEnv
 goto Exit
 
 :SetEnv
-set Qt_DIR=C:\Qt\Qt5.0.2\5.0.2
+set Qt_DIR=C:\Qt\5.1.1
 set NSIS_DIR=C:\Program Files (x86)\NSIS
+set MINGW_VER=mingw48_32
 
 if exist custom-windows.bat call custom-windows.bat
-set PATH=%Qt_DIR%\mingw47_32\bin;%Qt_DIR%\..\Tools\MinGW\bin;%NSIS_DIR%\Bin;%PATH%
+set PATH=%Qt_DIR%\%MINGW_VER%\bin;%Qt_DIR%\..\Tools\%MINGW_VER%\bin;%NSIS_DIR%\Bin;%PATH%
 
 :Exit
 echo on
