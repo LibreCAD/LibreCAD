58ebcfb0198d1875902c28b4d1f557879f109be3
Dongxu Li <dongxuli2011@gmail.com>
2013-11-13 01:53:50 -0800

    script to build for OSX versions

---
+++ scripts/build-dmg.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+#build dmg installers for OS/X
+
+OSNAME[6]="SnowLeopard"
+OSNAME[7]="Lion"
+OSNAME[8]="MountainLion"
+OSNAME[9]="Mavericks"
+
+SETTINGSFILE=../settings.pri
+
+for v in {6..9}
+do
+	if [ $v -gt 6 ]
+	then
+		sed -i'' -e '$d' $SETTINGSFILE
+	fi
+	echo "QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.$v" >> $SETTINGSFILE
+	./build-osx.sh
+	mv -v ../LibreCAD.dmg ../"LibreCAD-${OSNAME[$v]}".dmg
+done
+
