8d2ad72aa887c05e4462692f8327bba6325b36cd
Maher Hanafi <mhanafi@tamaggo.com>
2014-10-12 12:49:14 -0400

    win32: updated win32 scripts

--- scripts/build-win-setup.bat
+++ scripts/build-win-setup.bat
@@ -1,5 +1,5 @@
 call set-windows-env.bat
 
 cd postprocess-windows
-makensis.exe /X"SetCompressor /FINAL lzma" /V4 nsis-5.2.nsi
+makensis.exe /X"SetCompressor /FINAL lzma" /V4 nsis-5.3.nsi
 cd ..
--- scripts/build-windows.bat
+++ scripts/build-windows.bat
@@ -4,5 +4,10 @@ cd ..
 qmake.exe librecad.pro -r -spec win32-g++
 mingw32-make.exe clean
 mingw32-make.exe -j4
+if NOT exist windows\LibreCAD.exe (
+	echo "Building windows\LibreCAD.exe failed!"
+	exit /b /1
+)
+windeployqt.exe windows\LibreCAD.exe
 cd .\scripts
 call build-win-setup.bat
