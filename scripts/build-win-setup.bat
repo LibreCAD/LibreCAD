f4266972a20a0feea8d9bed17935f92cbc6d83d9
Dongxu Li <dongxuli2011@gmail.com>
2014-12-10 14:37:26 -0500

    win32: updated to Qt-5.4.0

--- scripts/build-win-setup.bat
+++ scripts/build-win-setup.bat
@@ -1,7 +1,7 @@
 call set-windows-env.bat
 
 if _%LC_NSIS_FILE%==_ (
-    set LC_NSIS_FILE=nsis-5.3.nsi
+    set LC_NSIS_FILE=nsis-5.4.nsi
 )
 pushd postprocess-windows
 makensis.exe /X"SetCompressor /FINAL lzma" /V4 %LC_NSIS_FILE%
