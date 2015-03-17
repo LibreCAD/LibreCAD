b52e6c733b77c7feb84156ba39d7f6cd66966720
Dongxu Li <dli@tamaggo.com>
2015-03-16 23:11:47 -0400

    file cleanup

--- librecad/src/lib/math/lc_quadratic.cpp
+++ librecad/src/lib/math/lc_quadratic.cpp
@@ -450,10 +450,10 @@ RS_VectorSolutions LC_Quadratic::getIntersection(const LC_Quadratic& l1, const L
         ce.push_back(p1->getCoefficients());
         ce.push_back(p2->getCoefficients());
         ret=RS_Math::simultaneousQuadraticSolverMixed(ce);
-        for(size_t j=0;j<ret.size();j++){
-            DEBUG_HEADER();
-            std::cout<<j<<": ("<<ret[j].x<<", "<< ret[j].y<<")"<<std::endl;
-        }
+//        for(size_t j=0;j<ret.size();j++){
+//            DEBUG_HEADER();
+//            std::cout<<j<<": ("<<ret[j].x<<", "<< ret[j].y<<")"<<std::endl;
+//        }
         return ret;
     }
     if( fabs(p1->m_mQuad(0,0))<RS_TOLERANCE && fabs(p1->m_mQuad(0,1))<RS_TOLERANCE
