9f5736637c5244fc075b00d635b062b4ead1a1d9
ravas <his_self@live.com>
2015-06-22 17:16:10 -0700

    switched to QRegExp for now

--- librecad/src/lib/gui/rs_eventhandler.cpp
+++ librecad/src/lib/gui/rs_eventhandler.cpp
@@ -25,7 +25,7 @@
 **********************************************************************/
 
 #include <QObject>
-#include <QRegularExpression>
+#include <QRegExp>
 
 #include "rs_eventhandler.h"
 #include "rs_actioninterface.h"
@@ -235,7 +235,7 @@ bool RS_EventHandler::cliCalculator(const QString& cmd) const
         return false;
     }
     // convert sin(45d) to sin(45*pi/180)
-    QRegularExpression regex(R"~(([\d\.]+)deg|d)~");
+    QRegExp regex(R"~(([\d\.]+)deg|d)~");
     str.replace(regex, R"~(\1*pi/180)~");
     bool ok=true;
     double result=RS_Math::eval(str,&ok);
