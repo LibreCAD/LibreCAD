b0f2515b08f2a547f1cd3d4203b275c3f25d367a
Miguel E. Hernández Cuervo <mhcuervo@gmail.com>
2015-07-15 15:40:29 -0400

    rscodec: Created new method calcDecode to separate out temporary decoding memory allocation/deallocation from the actual decoding.

--- libraries/libdxfrw/src/intern/rscodec.cpp
+++ libraries/libdxfrw/src/intern/rscodec.cpp
@@ -111,6 +111,204 @@ void RScodec::RSgen_poly() {
     for (i=0; i<=bb; i++)  gg[i] = index_of[gg[i]] ;
 }
 
+int RScodec::calcDecode(unsigned char* data, int* recd, int** elp, int* d, int* l, int* u_lu, int* s, int* root, int* loc, int* z, int* err, int* reg, int bb)
+{
+    if (!isOk) return -1;
+    int count = 0;
+    int syn_error = 0;
+    int i, j, u, q;
+
+    //    for (int i=0; i<nn; i++)
+    //       recd[i] = index_of[recd[i]] ;          /* put recd[i] into index form */
+    for (int i = 0, j = bb; i<kk; i++, j++)
+        recd[j] = index_of[data[j]];          /* put data in recd[i] into index form */
+    for (int i = kk, j = 0; i<nn; i++, j++)
+        recd[j] = index_of[data[j]];          /* put data in recd[i] into index form */
+
+    /* first form the syndromes */
+    for (i = 1; i <= bb; i++) {
+        s[i] = 0;
+        for (j = 0; j<nn; j++) {
+            if (recd[j] != -1) {
+                s[i] ^= alpha_to[(recd[j] + i*j) % nn];      /* recd[j] in index form */
+            }
+        }
+        /* convert syndrome from polynomial form to index form  */
+        if (s[i] != 0)  syn_error = 1;        /* set flag if non-zero syndrome => error */
+        s[i] = index_of[s[i]];
+    }
+
+    if (!syn_error) {      /* if no errors, ends */
+        /* no non-zero syndromes => no errors: output is received codeword */
+        return 0;
+    }
+
+    /* errors are present, try and correct */
+    /* compute the error location polynomial via the Berlekamp iterative algorithm,
+    following the terminology of Lin and Costello :   d[u] is the 'mu'th
+    discrepancy, where u='mu'+1 and 'mu' (the Greek letter!) is the step number
+    ranging from -1 to 2*tt (see L&C),  l[u] is the
+    degree of the elp at that step, and u_l[u] is the difference between the
+    step number and the degree of the elp.
+    */
+    /* initialise table entries */
+    d[0] = 0;           /* index form */
+    d[1] = s[1];        /* index form */
+    elp[0][0] = 0;      /* index form */
+    elp[1][0] = 1;      /* polynomial form */
+    for (i = 1; i<bb; i++) {
+        elp[0][i] = -1;   /* index form */
+        elp[1][i] = 0;   /* polynomial form */
+    }
+    l[0] = 0;
+    l[1] = 0;
+    u_lu[0] = -1;
+    u_lu[1] = 0;
+    u = 0;
+
+    do {
+        u++;
+        if (d[u] == -1) {
+            l[u + 1] = l[u];
+            for (i = 0; i <= l[u]; i++) {
+                elp[u + 1][i] = elp[u][i];
+                elp[u][i] = index_of[elp[u][i]];
+            }
+        }
+        else {
+            /* search for words with greatest u_lu[q] for which d[q]!=0 */
+            q = u - 1;
+            while ((d[q] == -1) && (q>0)) q--;
+            /* have found first non-zero d[q]  */
+            if (q>0) {
+                j = q;
+                do {
+                    j--;
+                    if ((d[j] != -1) && (u_lu[q]<u_lu[j]))
+                        q = j;
+                } while (j>0);
+            }
+
+            /* have now found q such that d[u]!=0 and u_lu[q] is maximum */
+            /* store degree of new elp polynomial */
+            if (l[u]>l[q] + u - q) {
+                l[u + 1] = l[u];
+            }
+            else {
+                l[u + 1] = l[q] + u - q;
+            }
+
+            /* form new elp(x) */
+            for (i = 0; i<bb; i++)    elp[u + 1][i] = 0;
+            for (i = 0; i <= l[q]; i++){
+                if (elp[q][i] != -1) {
+                    elp[u + 1][i + u - q] = alpha_to[(d[u] + nn - d[q] + elp[q][i]) % nn];
+                }
+            }
+            for (i = 0; i <= l[u]; i++) {
+                elp[u + 1][i] ^= elp[u][i];
+                elp[u][i] = index_of[elp[u][i]];  /*convert old elp value to index*/
+            }
+        }
+        u_lu[u + 1] = u - l[u + 1];
+
+        /* form (u+1)th discrepancy */
+        if (u<bb){    /* no discrepancy computed on last iteration */
+            if (s[u + 1] != -1) {
+                d[u + 1] = alpha_to[s[u + 1]];
+            }
+            else {
+                d[u + 1] = 0;
+            }
+            for (i = 1; i <= l[u + 1]; i++){
+                if ((s[u + 1 - i] != -1) && (elp[u + 1][i] != 0)) {
+                    d[u + 1] ^= alpha_to[(s[u + 1 - i] + index_of[elp[u + 1][i]]) % nn];
+                }
+            }
+            d[u + 1] = index_of[d[u + 1]];    /* put d[u+1] into index form */
+        }
+    } while ((u<bb) && (l[u + 1] <= tt));
+
+    u++;
+    if (l[u]>tt) {              /* elp has degree has degree >tt hence cannot solve */
+        return -1;              /* just output is received codeword as is */
+    }
+
+    /* can correct error */
+    /* put elp into index form */
+    for (i = 0; i <= l[u]; i++)   elp[u][i] = index_of[elp[u][i]];
+
+    /* find roots of the error location polynomial */
+    for (i = 1; i <= l[u]; i++) {
+        reg[i] = elp[u][i];
+    }
+    count = 0;
+    for (i = 1; i <= nn; i++) {
+        q = 1;
+        for (j = 1; j <= l[u]; j++) {
+            if (reg[j] != -1) {
+                reg[j] = (reg[j] + j) % nn;
+                q ^= alpha_to[reg[j]];
+            }
+        }
+        if (!q) {       /* store root and error location number indices */
+            root[count] = i;
+            loc[count] = nn - i;
+            count++;
+        }
+    }
+
+    if (count != l[u]) {   /* no. roots != degree of elp => >tt errors and cannot solve */
+        return -1;        /* just output is received codeword as is */
+    }
+
+    /* no. roots = degree of elp hence <= tt errors */
+    /* form polynomial z(x) */
+    for (i = 1; i <= l[u]; i++) {       /* Z[0] = 1 always - do not need */
+        if ((s[i] != -1) && (elp[u][i] != -1)) {
+            z[i] = alpha_to[s[i]] ^ alpha_to[elp[u][i]];
+        }
+        else if ((s[i] != -1) && (elp[u][i] == -1)) {
+            z[i] = alpha_to[s[i]];
+        }
+        else if ((s[i] == -1) && (elp[u][i] != -1)) {
+            z[i] = alpha_to[elp[u][i]];
+        }
+        else {
+            z[i] = 0;
+        }
+        for (j = 1; j<i; j++) {
+            if ((s[j] != -1) && (elp[u][i - j] != -1)) {
+                z[i] ^= alpha_to[(elp[u][i - j] + s[j]) % nn];
+            }
+        }
+        z[i] = index_of[z[i]];         /* put into index form */
+    }
+
+    /* evaluate errors at locations given by error location numbers loc[i] */
+    for (i = 0; i<nn; i++) err[i] = 0;
+    for (i = 0; i<l[u]; i++) {   /* compute numerator of error term first */
+        err[loc[i]] = 1;       /* accounts for z[0] */
+        for (j = 1; j <= l[u]; j++) {
+            if (z[j] != -1) {
+                err[loc[i]] ^= alpha_to[(z[j] + j*root[i]) % nn];
+            }
+        }
+        if (err[loc[i]] != 0) {
+            err[loc[i]] = index_of[err[loc[i]]];
+            q = 0;     /* form denominator of error term */
+            for (j = 0; j<l[u]; j++) {
+                if (j != i) {
+                    q += index_of[1 ^ alpha_to[(loc[j] + root[i]) % nn]];
+                }
+            }
+            q = q % nn;
+            err[loc[i]] = alpha_to[(err[loc[i]] - q + nn) % nn];
+            data[loc[i]] ^= err[loc[i]];  /*change errors by correct data, in polynomial form */
+        }
+    }
+    return count;
+}
 
 /** take the string of symbols in data[i], i=0..(k-1) and encode systematically
    to produce 2*tt parity symbols in bd[0]..bd[2*tt-1]
@@ -167,249 +365,24 @@ bool RScodec::encode(unsigned char *data, unsigned char *parity) {
 /** return value: number of corrected errors or -1 if can't correct it */
 int RScodec::decode(unsigned char *data) {
     if (!isOk) return -1;
-    int i,j,u,q ;
     int bb = nn-kk;; //nn-kk length of parity data
-    int *recd = new (std::nothrow) int[nn];
 
+    int *recd = new (std::nothrow) int[nn];
     int **elp = new int*[bb + 2];
     for (int i = 0; i < bb + 2; ++i)
         elp[i] = new int[bb];
-
     int *d = new int[bb + 2];
     int *l = new int[bb + 2];
     int *u_lu = new int[bb + 2];
     int *s = new int[bb + 1];
-    int count=0;
-    int syn_error=0;
     int *root = new int[tt];
     int *loc = new int[tt];
     int *z = new int[tt+1];
     int *err = new int[nn];
     int *reg = new int[tt + 1];
 
-//    for (int i=0; i<nn; i++)
-//       recd[i] = index_of[recd[i]] ;          /* put recd[i] into index form */
-    for (int i=0, j = bb; i<kk; i++,j++)
-       recd[j] = index_of[data[j]] ;          /* put data in recd[i] into index form */
-    for (int i=kk, j=0; i<nn; i++,j++)
-       recd[j] = index_of[data[j]] ;          /* put data in recd[i] into index form */
+    int res = calcDecode(data, recd, elp ,d ,l, u_lu, s, root, loc ,z, err, reg, bb);
 
-/* first form the syndromes */
-    for (i=1; i<=bb; i++) {
-        s[i] = 0 ;
-        for (j=0; j<nn; j++) {
-            if (recd[j]!=-1) {
-                s[i] ^= alpha_to[(recd[j]+i*j)%nn] ;      /* recd[j] in index form */
-            }
-        }
-        /* convert syndrome from polynomial form to index form  */
-        if (s[i]!=0)  syn_error=1 ;        /* set flag if non-zero syndrome => error */
-        s[i] = index_of[s[i]] ;
-    }
-
-    if (!syn_error) {      /* if no errors, ends */
-        /* no non-zero syndromes => no errors: output is received codeword */
-        delete[] recd;
-        for (int i = 0; i < bb + 2; ++i)
-            delete[] elp[i];
-        delete[] elp;
-        delete[] d;
-        delete[] l;
-        delete[] u_lu;
-        delete[] s;
-        delete[] root;
-        delete[] loc;
-        delete[] z;
-        delete[] err;
-        delete[] reg;
-        return 0;
-    }
-
-/* errors are present, try and correct */
-/* compute the error location polynomial via the Berlekamp iterative algorithm,
-   following the terminology of Lin and Costello :   d[u] is the 'mu'th
-   discrepancy, where u='mu'+1 and 'mu' (the Greek letter!) is the step number
-   ranging from -1 to 2*tt (see L&C),  l[u] is the
-   degree of the elp at that step, and u_l[u] is the difference between the
-   step number and the degree of the elp.
-*/
-/* initialise table entries */
-    d[0] = 0 ;           /* index form */
-    d[1] = s[1] ;        /* index form */
-    elp[0][0] = 0 ;      /* index form */
-    elp[1][0] = 1 ;      /* polynomial form */
-    for (i=1; i<bb; i++) {
-        elp[0][i] = -1 ;   /* index form */
-        elp[1][i] = 0 ;   /* polynomial form */
-    }
-    l[0] = 0 ;
-    l[1] = 0 ;
-    u_lu[0] = -1 ;
-    u_lu[1] = 0 ;
-    u = 0 ;
-
-    do {
-        u++ ;
-        if (d[u]==-1) {
-            l[u+1] = l[u] ;
-            for (i=0; i<=l[u]; i++) {
-                elp[u+1][i] = elp[u][i] ;
-                elp[u][i] = index_of[elp[u][i]] ;
-            }
-        } else {
-            /* search for words with greatest u_lu[q] for which d[q]!=0 */
-            q = u-1 ;
-            while ((d[q]==-1) && (q>0)) q-- ;
-            /* have found first non-zero d[q]  */
-            if (q>0) {
-                j=q ;
-                do {
-                    j-- ;
-                    if ((d[j]!=-1) && (u_lu[q]<u_lu[j]))
-                        q = j ;
-                } while (j>0) ;
-            }
-
-            /* have now found q such that d[u]!=0 and u_lu[q] is maximum */
-            /* store degree of new elp polynomial */
-            if (l[u]>l[q]+u-q) {
-                l[u+1] = l[u] ;
-            } else {
-                l[u+1] = l[q]+u-q;
-            }
-
-            /* form new elp(x) */
-            for (i=0; i<bb; i++)    elp[u+1][i] = 0 ;
-            for (i=0; i<=l[q]; i++){
-                if (elp[q][i]!=-1) {
-                    elp[u+1][i+u-q] = alpha_to[(d[u]+nn-d[q]+elp[q][i])%nn] ;
-                }
-            }
-            for (i=0; i<=l[u]; i++) {
-                elp[u+1][i] ^= elp[u][i] ;
-                elp[u][i] = index_of[elp[u][i]] ;  /*convert old elp value to index*/
-            }
-        }
-        u_lu[u+1] = u-l[u+1] ;
-
-        /* form (u+1)th discrepancy */
-        if (u<bb){    /* no discrepancy computed on last iteration */
-            if (s[u+1]!=-1) {
-                d[u+1] = alpha_to[s[u+1]] ;
-            } else {
-                d[u+1] = 0 ;
-            }
-            for (i=1; i<=l[u+1]; i++){
-                if ((s[u+1-i]!=-1) && (elp[u+1][i]!=0)) {
-                    d[u+1] ^= alpha_to[(s[u+1-i]+index_of[elp[u+1][i]])%nn] ;
-                }
-            }
-            d[u+1] = index_of[d[u+1]] ;    /* put d[u+1] into index form */
-        }
-    } while ((u<bb) && (l[u+1]<=tt)) ;
-
-    u++ ;
-    if (l[u]>tt) {                 /* elp has degree has degree >tt hence cannot solve */
-        delete[] recd;        /* just output is received codeword as is */
-        for (int i = 0; i < bb + 2; ++i)
-            delete[] elp[i];
-        delete[] elp;
-        delete[] d;
-        delete[] l;
-        delete[] u_lu;
-        delete[] s;
-        delete[] root;
-        delete[] loc;
-        delete[] z;
-        delete[] err;
-        delete[] reg;
-        return -1;
-    }
-
-    /* can correct error */
-    /* put elp into index form */
-    for (i=0; i<=l[u]; i++)   elp[u][i] = index_of[elp[u][i]] ;
-
-    /* find roots of the error location polynomial */
-    for (i=1; i<=l[u]; i++) {
-        reg[i] = elp[u][i] ;
-    }
-    count = 0 ;
-    for (i=1; i<=nn; i++) {
-        q = 1 ;
-        for (j=1; j<=l[u]; j++) {
-            if (reg[j]!=-1) {
-                reg[j] = (reg[j]+j)%nn ;
-                q ^= alpha_to[reg[j]] ;
-            }
-        }
-        if (!q) {       /* store root and error location number indices */
-            root[count] = i;
-            loc[count] = nn-i ;
-            count++ ;
-        }
-    }
-
-
-    if (count!=l[u]) {   /* no. roots != degree of elp => >tt errors and cannot solve */
-        delete[] recd;        /* just output is received codeword as is */
-        for (int i = 0; i < bb + 2; ++i)
-            delete[] elp[i];
-        delete[] elp;
-        delete[] d;
-        delete[] l;
-        delete[] u_lu;
-        delete[] s;
-        delete[] root;
-        delete[] loc;
-        delete[] z;
-        delete[] err;
-        delete[] reg;
-        return -1;
-    }
-
-    /* no. roots = degree of elp hence <= tt errors */
-    /* form polynomial z(x) */
-    for (i=1; i<=l[u]; i++) {       /* Z[0] = 1 always - do not need */
-        if ((s[i]!=-1) && (elp[u][i]!=-1)) {
-            z[i] = alpha_to[s[i]] ^ alpha_to[elp[u][i]] ;
-        } else if ((s[i]!=-1) && (elp[u][i]==-1)) {
-            z[i] = alpha_to[s[i]] ;
-        } else if ((s[i]==-1) && (elp[u][i]!=-1)) {
-            z[i] = alpha_to[elp[u][i]] ;
-         } else {
-            z[i] = 0;
-        }
-        for (j=1; j<i; j++) {
-            if ((s[j]!=-1) && (elp[u][i-j]!=-1)) {
-                z[i] ^= alpha_to[(elp[u][i-j] + s[j])%nn];
-            }
-        }
-        z[i] = index_of[z[i]] ;         /* put into index form */
-    }
-
-    /* evaluate errors at locations given by error location numbers loc[i] */
-    for (i=0; i<nn; i++) err[i] = 0;
-    for (i=0; i<l[u]; i++) {   /* compute numerator of error term first */
-        err[loc[i]] = 1;       /* accounts for z[0] */
-        for (j=1; j<=l[u]; j++) {
-            if (z[j]!=-1) {
-                err[loc[i]] ^= alpha_to[(z[j]+j*root[i])%nn] ;
-            }
-        }
-        if (err[loc[i]]!=0) {
-            err[loc[i]] = index_of[err[loc[i]]] ;
-            q = 0 ;     /* form denominator of error term */
-            for (j=0; j<l[u]; j++) {
-                if (j!=i) {
-                    q += index_of[1^alpha_to[(loc[j]+root[i])%nn]] ;
-                }
-            }
-            q = q % nn ;
-            err[loc[i]] = alpha_to[(err[loc[i]]-q+nn)%nn] ;
-            data[loc[i]] ^= err[loc[i]] ;  /*change errors by correct data, in polynomial form */
-        }
-    }
     delete[] recd;
     for (int i = 0; i < bb + 2; ++i)
         delete[] elp[i];
@@ -423,5 +396,6 @@ int RScodec::decode(unsigned char *data) {
     delete[] z;
     delete[] err;
     delete[] reg;
-    return count;
+
+    return res;
 }
