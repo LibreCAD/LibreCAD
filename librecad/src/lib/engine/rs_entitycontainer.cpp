LibreCAD/LibreCAD

commit 527cbd4098d1d9c043ef06cd4e2a31816f5c09cd

entity container: header cleanup

librecad/src/lib/engine/rs_entitycontainer.cpp
@@ -490,14 +490,16 @@ unsigned int RS_EntityContainer::countDeep() const{
 /**
  * Counts the selected entities in this container.
  */
-unsigned int RS_EntityContainer::countSelected(bool deep, std::set<RS2::EntityType> const& types) {
+unsigned int RS_EntityContainer::countSelected(bool deep, std::initializer_list<RS2::EntityType> const& types) {
     unsigned int c=0;
 
 	for (RS_Entity* t: entities){
 
 		if (t->isSelected()) {
 
-			if(!types.size() || types.count(t->rtti()))
+			if(!types.size() ||
+					std::find(types.begin(), types.end(), t->rtti()) != types.end()
+					)
 				c++;
         }
 		if(t->isContainer())

librecad/src/lib/engine/rs_blocklist.cpp
@@ -24,6 +24,7 @@
 **
 **********************************************************************/
 
+#include <set>
 #include <QString>
 #include <QRegExp>
 #include "rs_debug.h"
@@ -212,6 +213,7 @@ void RS_BlockList::editBlock(RS_Block* block, const RS_Block& source) {
 RS_Block* RS_BlockList::find(const QString& name) {
     //RS_DEBUG->print("RS_BlockList::find");
 	// Todo : reduce this from O(N) to O(log(N)) complexity based on sorted list or hash
+	//DFS
 	std::vector<RS_BlockList const*> nodes;
 	std::set<RS_BlockList const*> searched;
 	searched.insert(nullptr);

librecad/src/lib/engine/rs_entitycontainer.h
@@ -29,7 +29,6 @@
 #define RS_ENTITYCONTAINER_H
 
 #include <vector>
-#include <set>
 #include "rs_entity.h"
 
 /**
@@ -121,7 +120,7 @@ public:
 	* @param deep count sub-containers, if true
 	* @param types if is not empty, only counts by types listed
 	*/
-	virtual unsigned countSelected(bool deep=true, std::set<RS2::EntityType> const& types = std::set<RS2::EntityType>());
+	virtual unsigned countSelected(bool deep=true, std::initializer_list<RS2::EntityType> const& types = {});
     virtual double totalSelectedLength();
 
     /**

commit 3fa99330ce2072e6c356db67ee5fb9009d6cc795

avoid potential performance regression by 527cbd40

librecad/src/lib/engine/rs_entitycontainer.cpp
@@ -25,8 +25,9 @@
 **********************************************************************/
 
 
-#include <QObject>
 #include <cmath>
+#include <set>
+#include <QObject>
 
 #include "rs_dialogfactory.h"
 #include "qg_dialogfactory.h"
@@ -492,17 +493,15 @@ unsigned int RS_EntityContainer::countDeep() const{
  */
 unsigned int RS_EntityContainer::countSelected(bool deep, std::initializer_list<RS2::EntityType> const& types) {
     unsigned int c=0;
+	std::set<RS2::EntityType> type = types;
 
 	for (RS_Entity* t: entities){
 
-		if (t->isSelected()) {
-
-			if(!types.size() ||
-					std::find(types.begin(), types.end(), t->rtti()) != types.end()
-					)
+		if (t->isSelected())
+			if (!types.size() || type.count(t->rtti()))
 				c++;
-        }
-		if(t->isContainer())
+
+		if (t->isContainer())
 			c += static_cast<RS_EntityContainer*>(t)->countSelected(deep);
     }
 