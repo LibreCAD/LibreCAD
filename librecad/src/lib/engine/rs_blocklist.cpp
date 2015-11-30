527cbd4098d1d9c043ef06cd4e2a31816f5c09cd
Dongxu Li <dongxuli2011@gmail.com>
2015-11-30 13:40:58 -0500

    entity container: header cleanup

--- librecad/src/lib/engine/rs_blocklist.cpp
+++ librecad/src/lib/engine/rs_blocklist.cpp
@@ -24,6 +24,7 @@
 **
 **********************************************************************/
 
+#include <set>
 #include <QString>
 #include <QRegExp>
 #include "rs_debug.h"
@@ -212,6 +213,7 @@ void RS_BlockList::editBlock(RS_Block* block, const RS_Block& source) {
 RS_Block* RS_BlockList::find(const QString& name) {
     //RS_DEBUG->print("RS_BlockList::find");
 	// Todo : reduce this from O(N) to O(log(N)) complexity based on sorted list or hash
+	//DFS
 	std::vector<RS_BlockList const*> nodes;
 	std::set<RS_BlockList const*> searched;
 	searched.insert(nullptr);
