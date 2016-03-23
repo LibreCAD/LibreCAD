a96b4650905d4533b91cebe2c6c1caf62a9da071
Dongxu Li <dongxuli2011@gmail.com>
2016-03-23 20:28:02 -0300

    circle: specify override for derived

--- librecad/src/lib/engine/rs_circle.h
+++ librecad/src/lib/engine/rs_circle.h
@@ -60,14 +60,14 @@ public:
                const RS_CircleData& d);
 	~RS_Circle() = default;
 
-	virtual RS_Entity* clone() const;
+	RS_Entity* clone() const override;
 
     /**	@return RS2::EntityCircle */
-    virtual RS2::EntityType rtti() const {
+	RS2::EntityType rtti() const override{
         return RS2::EntityCircle;
     }
     /** @return true */
-    virtual bool isEdge() const {
+	bool isEdge() const  override{
         return true;
     }
 
@@ -76,37 +76,37 @@ public:
         return data;
     }
 
-	virtual RS_VectorSolutions getRefPoints() const;
+	RS_VectorSolutions getRefPoints() const override;
 
     //no start/end point for whole circle
-    //        virtual RS_Vector getStartpoint() const {
+	//        RS_Vector getStartpoint() const {
     //                return data.center + RS_Vector(data.radius, 0.0);
     //        }
-    //        virtual RS_Vector getEndpoint() const {
+	//        RS_Vector getEndpoint() const {
     //                return data.center + RS_Vector(data.radius, 0.0);
     //        }
     /**
          * @return Direction 1. The angle at which the arc starts at
          * the startpoint.
          */
-    double getDirection1() const; 
+	double getDirection1() const override;
     /**
          * @return Direction 2. The angle at which the arc starts at
          * the endpoint.
          */
-    double getDirection2() const;
+	double getDirection2() const override;
 
     /** @return The center point (x) of this arc */
-	virtual RS_Vector getCenter() const;
+	RS_Vector getCenter() const override;
     /** Sets new center. */
 	void setCenter(const RS_Vector& c);
     /** @return The radius of this arc */
-	virtual double getRadius() const;
+	double getRadius() const override;
     /** Sets new radius. */
 	void setRadius(double r);
     double getAngleLength() const;
-    virtual double getLength() const;
-	virtual bool isTangent(const RS_CircleData&  circleData) const;
+	double getLength() const override;
+	bool isTangent(const RS_CircleData&  circleData) const override;
 
     bool createFromCR(const RS_Vector& c, double r);
     bool createFrom2P(const RS_Vector& p1, const RS_Vector& p2);
@@ -114,7 +114,7 @@ public:
                       const RS_Vector& p3);
     bool createFrom3P(const RS_VectorSolutions& sol);
 	bool createInscribe(const RS_Vector& coord, const std::vector<RS_Line*>& lines);
-	virtual std::vector<RS_Entity* > offsetTwoSides(const double& distance) const;
+	std::vector<RS_Entity* > offsetTwoSides(const double& distance) const override;
 	RS_VectorSolutions createTan1_2P(const RS_AtomicEntity* circle, const std::vector<RS_Vector>& points);
 	static RS_VectorSolutions createTan2(const std::vector<RS_AtomicEntity*>& circles, const double& r);
     /** solve one of the eight Appollonius Equations
@@ -125,37 +125,37 @@ with Cx the center of the common tangent circle, Rx the radius. Ci and Ri are th
 
 	std::vector<RS_Circle> createTan3(const std::vector<RS_AtomicEntity*>& circles);
 	bool testTan3(const std::vector<RS_AtomicEntity*>& circles);
-    virtual RS_Vector getMiddlePoint(void)const;
-    virtual RS_Vector getNearestEndpoint(const RS_Vector& coord,
-                                         double* dist = nullptr) const;
-    virtual RS_Vector getNearestPointOnEntity(const RS_Vector& coord,
-                                              bool onEntity = true, double* dist = NULL, RS_Entity** entity=NULL)const;
-    virtual RS_Vector getNearestCenter(const RS_Vector& coord,
-									   double* dist = NULL)const;
-    virtual RS_Vector getNearestMiddle(const RS_Vector& coord,
+	RS_Vector getMiddlePoint(void)const override;
+	RS_Vector getNearestEndpoint(const RS_Vector& coord,
+										 double* dist = nullptr) const override;
+	RS_Vector getNearestPointOnEntity(const RS_Vector& coord,
+											  bool onEntity = true, double* dist = NULL, RS_Entity** entity=NULL)const override;
+	RS_Vector getNearestCenter(const RS_Vector& coord,
+									   double* dist = NULL)const override;
+	RS_Vector getNearestMiddle(const RS_Vector& coord,
                                        double* dist = nullptr,
-                                       int middlePoints = 1 ) const;
-    virtual RS_Vector getNearestDist(double distance,
+									   int middlePoints = 1 ) const override;
+	RS_Vector getNearestDist(double distance,
                                      const RS_Vector& coord,
-									 double* dist = NULL)const;
-    virtual RS_Vector getNearestDist(double distance,
-									 bool startp)const;
-    virtual RS_Vector getNearestOrthTan(const RS_Vector& coord,
+									 double* dist = NULL)const override;
+	RS_Vector getNearestDist(double distance,
+									 bool startp)const override;
+	RS_Vector getNearestOrthTan(const RS_Vector& coord,
                                         const RS_Line& normal,
-										bool onEntity = false) const;
+										bool onEntity = false) const override;
 
-    virtual bool offset(const RS_Vector& coord, const double& distance);
-    virtual RS_VectorSolutions getTangentPoint(const RS_Vector& point) const;//find the tangential points seeing from given point
-    virtual RS_Vector getTangentDirection(const RS_Vector& point)const;
-    virtual void move(const RS_Vector& offset);
-    virtual void rotate(const RS_Vector& center, const double& angle);
-    virtual void rotate(const RS_Vector& center, const RS_Vector& angleVector);
-    virtual void scale(const RS_Vector& center, const RS_Vector& factor);
-    virtual void mirror(const RS_Vector& axisPoint1, const RS_Vector& axisPoint2);
-    virtual void moveRef(const RS_Vector& ref, const RS_Vector& offset);
+	bool offset(const RS_Vector& coord, const double& distance) override;
+	RS_VectorSolutions getTangentPoint(const RS_Vector& point) const override;//find the tangential points seeing from given point
+	RS_Vector getTangentDirection(const RS_Vector& point)const override;
+	void move(const RS_Vector& offset) override;
+	void rotate(const RS_Vector& center, const double& angle) override;
+	void rotate(const RS_Vector& center, const RS_Vector& angleVector) override;
+	void scale(const RS_Vector& center, const RS_Vector& factor) override;
+	void mirror(const RS_Vector& axisPoint1, const RS_Vector& axisPoint2) override;
+	void moveRef(const RS_Vector& ref, const RS_Vector& offset) override;
     /** whether the entity's bounding box intersects with visible portion of graphic view */
-    virtual bool isVisibleInWindow(RS_GraphicView* view) const;
-    virtual void draw(RS_Painter* painter, RS_GraphicView* view, double& patternOffset);
+	bool isVisibleInWindow(RS_GraphicView* view) const override;
+	void draw(RS_Painter* painter, RS_GraphicView* view, double& patternOffset) override;
     /** return the equation of the entity
 for quadratic,
 
@@ -165,7 +165,7 @@ m0 x^2 + m1 xy + m2 y^2 + m3 x + m4 y + m5 =0
 for linear:
 m0 x + m1 y + m2 =0
 **/
-    virtual LC_Quadratic getQuadratic() const;
+	LC_Quadratic getQuadratic() const override;
     
 /**
 * @brief Returns area of full circle
@@ -173,11 +173,11 @@ m0 x + m1 y + m2 =0
 * However, full ellipses and ellipse arcs are handled by RS_Ellipse
 * @return \pi r^2
 */
-    virtual double areaLineIntegral() const override;
+	double areaLineIntegral() const override;
 
     friend std::ostream& operator << (std::ostream& os, const RS_Circle& a);
 
-    virtual void calculateBorders();
+	void calculateBorders() override;
 
 protected:
     RS_CircleData data;
