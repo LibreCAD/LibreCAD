4a60c02bd3959b406decd33835b97a899eeb37b1
ravas <ravas@outlook.com>
2016-10-06 16:01:06 -0700

    removed my old address
    investigate tuta.io
    ^_^

--- librecad/src/main/mainwindowx.cpp
+++ librecad/src/main/mainwindowx.cpp
@@ -3,7 +3,7 @@
 **
 ** This file was created for the LibreCAD project (librecad.org), a 2D CAD program.
 **
-** Copyright (C) 2015 ravas (ravas@outlook.com - github.com/r-a-v-a-s)
+** Copyright (C) 2015 ravas (github.com/r-a-v-a-s)
 **
 ** This program is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU General Public License
--- librecad/src/ui/generic/colorcombobox.cpp
+++ librecad/src/ui/generic/colorcombobox.cpp
@@ -3,7 +3,7 @@
 **
 ** This file was created for the LibreCAD project (librecad.org), a 2D CAD program.
 **
-** Copyright (C) 2016 ravas (ravas@outlook.com)
+** Copyright (C) 2016 ravas (github.com/r-a-v-a-s)
 **
 ** This program is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU General Public License
