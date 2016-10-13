cd455f7d8198749b17c127eb4fa3399a35f96346
ravas <ravas@tuta.io>
2016-10-13 00:36:51 -0700

    added font folder selection button
    * qg_dlgoptionsgeneral.cpp/h (setFontsFolder): new function
    * qg_dlgoptionsgeneral.ui (fonts_button): new QToolButton
    (btTemplate, variablefile_button): switched to open.svg

--- librecad/src/ui/forms/qg_dlgoptionsgeneral.h
+++ librecad/src/ui/forms/qg_dlgoptionsgeneral.h
@@ -4,7 +4,7 @@
 **
 ** Copyright (C) 2010 R. van Twisk (librecad@rvt.dds.nl)
 ** Copyright (C) 2001-2003 RibbonSoft. All rights reserved.
-**
+** Copyright (C) 2015, 2016 ravas (github.com/r-a-v-a-s)
 **
 ** This file may be distributed and/or modified under the terms of the
 ** GNU General Public License version 2 as published by the Free Software 
@@ -72,6 +72,8 @@ private slots:
     void on_pb_snap_color_clicked();
 
     void setVariableFile();
+
+    void setFontsFolder();
 
 private:
     bool restartNeeded;
