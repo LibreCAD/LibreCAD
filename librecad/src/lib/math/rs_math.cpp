7e0ede9d570a9894e9febeda41efeed3f8466386
webmite <webmite@users.noreply.github.com>
2017-04-04 16:17:26 -0400

    Update rs_math.cpp eval fn code change
    corrected QString indexOf call by adding Case Sensitivity argument in passed variables.

--- librecad/src/lib/math/rs_math.cpp
+++ librecad/src/lib/math/rs_math.cpp
@@ -246,7 +246,7 @@ void replaceAll(QString& str, const std::string& from, const std::string& to) {
     if(from.empty())
         return;
     size_t start_pos = 0;
-    while((start_pos = str.indexOf(from, start_pos)) != std::string::npos) {
+    while((start_pos = str.indexOf(from, start_pos,0)) != std::string::npos) {
         str.replace(start_pos, from.length(), to);
         start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
     }
