66a8bc34ba8ee7eb4fb849c64ef367f0b3123c24
Luis Colorado <luiscoloradourcola@gmail.com>
2017-11-09 22:16:54 +0200

    One step more towards finding the common point
    between the carving evolute and the tooth profile one.
    
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- plugins/gear/gear.cpp
+++ plugins/gear/gear.cpp
@@ -172,13 +172,6 @@ lc_Geardlg::lc_Geardlg(QWidget *parent) :
     INFO("END\n");
 }
 
-/* calculate the offset angle to rotate the gear so the
- * tooth first face pitch point lies on the X axis. */
-static double offset(const double p_angle)
-{
-    return tan(p_angle) - p_angle;
-}
-
 /* calculate the radius of a point in canonical evoluta
  * whose radius is given. */
 static double radius2arg(const double radius, const double alpha = 0.0)
@@ -201,6 +194,7 @@ static double im_evolute(const double phi, const double alpha = 0.0)
     return (1.0 - alpha) * sin(phi) - phi * cos(phi);
 }
 
+#if 0
 /* modulus of evolute at point phi */
 static double mod_evolute(const double phi, const double alpha = 0.0)
 {
@@ -208,7 +202,6 @@ static double mod_evolute(const double phi, const double alpha = 0.0)
     return sqrt(aux*aux + phi*phi);
 }
 
-#if 0
 /* finds the root of a function given two points.  The algorithm treats to follow the line
  * that joins the two images of the points a and b, until it crosses the X axis.  Then the
  * function is evaluated on the found point and this value is used to substitute and discard
@@ -216,11 +209,11 @@ static double mod_evolute(const double phi, const double alpha = 0.0)
  * parameter supplied (or defaulted to 1.0e-13) the algorithm terminates.  This is expected
  * to work as the value to be found is for a function that is warranted to have a zero and
  */
-static double find_root(double (*f)(double), double a, double b, const double eps = 1.0e-13)
+static double find_root(const evolute& function, double a, double b, const double eps = 1.0e-13)
 {
-    double f_a = f(a), f_b = f(b);
+    double f_a = function(a), f_b = function(b);
     do {
-        const double x = (a*f_b - b*f_a) / (f_b - f_a), f_x = f(x);
+        const double x = (a*f_b - b*f_a) / (f_b - f_a), f_x = function(x);
         if (fabs(x - a) < fabs(x - b)) {
             b = x; f_b = f_x;
         } else {
@@ -229,7 +222,59 @@ static double find_root(double (*f)(double), double a, double b, const double ep
     } while (fabs(a-b) > eps);
     return a;
 } /* find_root */
+
 #endif
+
+struct evolute {
+    evolute(int n_t, double add, double ded, double p_ang);
+
+    const int n_teeth;
+    const double
+        addendum, dedendum,
+        c_modulus,
+        p_angle, cos_p_angle,
+        off_rot, cos_off_rot, sin_off_rot,
+        dedendum_radius, addendum_radius,
+        phi_at_dedendum, phi_at_addendum,
+        alpha, angle_0;
+    double angle_1;
+    double operator()(double phi);
+};
+
+evolute::evolute(int n_t, double add, double ded, double p_ang):
+    n_teeth(n_t),
+    addendum(add), dedendum(ded),
+    c_modulus(2.0/n_t), 
+    p_angle(p_ang), cos_p_angle(cos(p_ang)),
+    off_rot(tan(p_angle) - p_angle), cos_off_rot(cos(off_rot)),sin_off_rot(sin(off_rot)),
+    dedendum_radius(1.0 - c_modulus * dedendum),
+    addendum_radius(1.0 + c_modulus * addendum),
+    phi_at_dedendum(dedendum_radius > cos_p_angle ? radius2arg(dedendum_radius / cos_p_angle) : 0.0),
+    phi_at_addendum(radius2arg(addendum_radius / cos_p_angle)),
+    alpha(1.0 - dedendum_radius),
+    angle_0(alpha * tan(p_angle))
+{
+    P(n_teeth);
+    P(addendum);
+    P(dedendum);
+    P(c_modulus);
+    P(p_angle);
+    P(cos_p_angle);
+    P(off_rot);
+    P(cos_off_rot);
+    P(sin_off_rot);
+    P(dedendum_radius);
+    P(addendum_radius);
+    P(phi_at_dedendum);
+    P(phi_at_addendum);
+    P(alpha);
+    P(angle_0);
+}
+
+double evolute::operator()(double phi)
+{
+       return phi;
+}
 
 void lc_Geardlg::processAction(Document_Interface *doc, const QString& cmd, QPointF& center)
 {
@@ -255,95 +300,68 @@ void lc_Geardlg::processAction(Document_Interface *doc, const QString& cmd, QPoi
      * Finally, we get the complete set of teeth by rotating them by the
      * pitch angular modulus to get the whole gear */
 
-    const int    n_teeth = nteethBox->value();
-    const double addendum = addendumBox->value();
-    const double dedendum = dedendumBox->value();
+    evolute ev(nteethBox->value(), /* number of teeth */
+               addendumBox->value(), /* addendum */
+               dedendumBox->value(), /* dedendum */
+               M_PI / 180.0 * pressureBox->value()); /* pressure angle (converted to rad) */
+
     const double modulus = modulusBox->value();
-    const double c_modulus = 2.0/n_teeth; /* canonical modulus (on a 1.0 pitch circle radius) */
-    const double p_angle = M_PI / 180.0 * pressureBox->value();
-    const double cos_p_angle = cos(p_angle);
-    const double scale_factor = cos_p_angle * modulus / c_modulus;
-    const double off_rot = offset(p_angle); /* rot off. to get the pitch point aligned to X axis */
-    const double cos_off_rot = cos(off_rot);
-    const double sin_off_rot = sin(off_rot);
-    const double pitch_radius = mod_evolute(p_angle + off_rot);
-    const double dedendum_radius = pitch_radius - c_modulus * dedendum / cos_p_angle;
-    const double addendum_radius = pitch_radius + c_modulus * addendum / cos_p_angle;
-    const double phi_at_dedendum = (dedendum_radius >= 1.0)
-                                 ? radius2arg(dedendum_radius)
-                                 : 0.0;
-    const double phi_at_addendum = radius2arg(addendum_radius);
+    const double scale_factor = modulus / ev.c_modulus;
     const int    n1 = n1Box->value();
     const int    n2 = n2Box->value();
     const double rotation = rotateBox->value() * M_PI / 180.0;
+
+    P(modulus);
+    P(scale_factor);
+    P(n1);
+    P(n2);
+    P(rotation);
 
     rotate_and_disp = rotate_and_disp
         .translate(center.x(), center.y())
         .rotateRadians(rotation);
 
-    P(n_teeth);
-    P(addendum);
-    P(dedendum);
-    P(modulus);
-    P(c_modulus);
-    P(p_angle);
-    P(cos_p_angle);
-    P(scale_factor);
-    P(off_rot);
-    P(cos_off_rot);
-    P(sin_off_rot);
-    P(pitch_radius);
-    P(dedendum_radius);
-    P(addendum_radius);
-    P(phi_at_dedendum);
-    P(phi_at_addendum);
-    P(n1);
-    P(n2);
-    P(rotation);
+
     /* Build one tooth face */
-    if (dedendum_radius < 1.0) {
-        if (calcInterferenceBox->isChecked() && pitch_radius * cos_p_angle * cos_p_angle > dedendum_radius) {
-            /* TODO: I'm here coding. */
-            const int n3 = n3Box->value();
-            const double alpha = 1.0 - dedendum_radius / pitch_radius;
-            const double angle_0 = alpha * tan(p_angle);
-            const double angle_1 = radius2arg(cos_p_angle, alpha);
-            P(n3);
-            P(alpha);
-            P(angle_0);
-            P(angle_1);
-            int i;
-            double phi, delta = angle_1 / n3;
-            for(i = 0, phi = 0.0; i <= n3; i++, phi -= delta) {
-                const double re = scale_factor * re_evolute(phi, alpha) * pitch_radius;
-                const double im = scale_factor * im_evolute(phi, alpha) * pitch_radius;
-                const double cos_rot = cos(angle_0), sin_rot = sin(angle_0);
-                const QPointF rot( cos_rot * re + sin_rot * im,
-                                  -sin_rot * re + cos_rot * im);
-                first_tooth.push_back(rot);
-                polyline.push_back(Plug_VertexData(rotate_and_disp.map(rot), 0.0));
-            } /* for */
-        } else {
-            QPointF root( scale_factor * dedendum_radius * cos_off_rot,
-                         -scale_factor * dedendum_radius * sin_off_rot);
-            first_tooth.push_back(root);
-            polyline.push_back(Plug_VertexData(rotate_and_disp.map(root), 0.0));
-        }
+    if (calcInterferenceBox->isChecked() && ev.cos_p_angle * ev.cos_p_angle > ev.dedendum_radius) {
+        /* TODO: I'm here coding. */
+        const int n3 = n3Box->value();
+        const double angle_1_a = radius2arg(ev.cos_p_angle, ev.alpha); /* phi @ base */
+        const double angle_1_b = radius2arg(1.0, ev.alpha); /* phi @ pitch */
+        P(n3);
+        P(angle_1_a);
+        P(angle_1_b);
+        int i;
+        double phi, delta = angle_1_a / n3;
+        for(i = 0, phi = 0.0; i <= n3; i++, phi -= delta) {
+            const double re = scale_factor * re_evolute(phi, ev.alpha);
+            const double im = scale_factor * im_evolute(phi, ev.alpha);
+            const double cos_rot = cos(ev.angle_0), sin_rot = sin(ev.angle_0);
+            const QPointF rot( cos_rot * re + sin_rot * im,
+                              -sin_rot * re + cos_rot * im);
+            first_tooth.push_back(rot);
+            polyline.push_back(Plug_VertexData(rotate_and_disp.map(rot), 0.0));
+        } /* for */
+    } else {
+        QPointF root( scale_factor * ev.dedendum_radius * ev.cos_off_rot,
+                     -scale_factor * ev.dedendum_radius * ev.sin_off_rot);
+        first_tooth.push_back(root);
+        polyline.push_back(Plug_VertexData(rotate_and_disp.map(root), 0.0));
     }
 
     int i;
     const int N = n1 + n2;
     double phi;
-    double delta = (p_angle + off_rot - phi_at_dedendum) / n1;
-    for (i = 0, phi = phi_at_dedendum; i <= N; ++i, phi += delta) {
-        QPointF point( scale_factor * re_evolute(phi),
-                       scale_factor * im_evolute(phi)),
-                rot_point( cos_off_rot * point.x() + sin_off_rot * point.y(),
-                          -sin_off_rot * point.x() + cos_off_rot * point.y());
+    double delta = (ev.p_angle + ev.off_rot - ev.phi_at_dedendum) / n1;
+    for (i = 0, phi = ev.phi_at_dedendum; i <= N; ++i, phi += delta) {
+        QPointF point( ev.cos_p_angle * scale_factor * re_evolute(phi),
+                       ev.cos_p_angle * scale_factor * im_evolute(phi)),
+                rot_point( ev.cos_off_rot * point.x() + ev.sin_off_rot * point.y(),
+                          -ev.sin_off_rot * point.x() + ev.cos_off_rot * point.y());
         first_tooth.push_back(rot_point);
         polyline.push_back(Plug_VertexData(rotate_and_disp.map(rot_point), 0.0));
         if (i == n1) { /* change delta to continue until we have all points */
-            delta = (phi_at_addendum - p_angle - off_rot) / n2;
+            delta = (ev.phi_at_addendum - ev.p_angle - ev.off_rot) / n2;
         }
     } /* for */
 
@@ -356,7 +374,7 @@ void lc_Geardlg::processAction(Document_Interface *doc, const QString& cmd, QPoi
      * (note: we don't use iterators as the array is growing as long as we
      * navigate it)
      */
-    const double axis_angle_x_2 = M_PI / n_teeth;
+    const double axis_angle_x_2 = M_PI / ev.n_teeth;
     const double axis_angle = axis_angle_x_2 / 2.0;
     const double cos_axis_angle_x_2 = cos(axis_angle_x_2);
     const double sin_axis_angle_x_2 = sin(axis_angle_x_2);
@@ -365,8 +383,8 @@ void lc_Geardlg::processAction(Document_Interface *doc, const QString& cmd, QPoi
     const double n_to_mirror = first_tooth.size();
 
     /* symmetry axis point (at top of tooth) */
-    QPointF mirror_point(scale_factor * addendum_radius * cos(axis_angle),
-                         scale_factor * addendum_radius * sin(axis_angle));
+    QPointF mirror_point(scale_factor * ev.addendum_radius * cos(axis_angle),
+                         scale_factor * ev.addendum_radius * sin(axis_angle));
     first_tooth.push_back(mirror_point);
     polyline.push_back(Plug_VertexData(rotate_and_disp.map(mirror_point), 0.0));
 
@@ -381,15 +399,15 @@ void lc_Geardlg::processAction(Document_Interface *doc, const QString& cmd, QPoi
     } /* for */
 
     /* symmetry axis point (at interteeth) */
-    QPointF mirror_point2(scale_factor * dedendum_radius * cos(axis_angle + axis_angle_x_2),
-                          scale_factor * dedendum_radius * sin(axis_angle + axis_angle_x_2));
+    QPointF mirror_point2(scale_factor * ev.dedendum_radius * cos(axis_angle + axis_angle_x_2),
+                          scale_factor * ev.dedendum_radius * sin(axis_angle + axis_angle_x_2));
     first_tooth.push_back(mirror_point2);
     polyline.push_back(Plug_VertexData(rotate_and_disp.map(mirror_point2), 0.0));
 
     /* now, we have to rotate the tooth to get all the teeth missing. */
-    for (i = 1; i < n_teeth; i++) {
+    for (i = 1; i < ev.n_teeth; i++) {
 
-        const double angle = M_PI * c_modulus * i;
+        const double angle = M_PI * ev.c_modulus * i;
         const double cos_angle = cos(angle);
         const double sin_angle = sin(angle);
 
@@ -420,30 +438,30 @@ void lc_Geardlg::processAction(Document_Interface *doc, const QString& cmd, QPoi
 
     if (drawPitchCircleBox->isChecked()) {
         LAYER("pitch_circles");
-        doc->addCircle(&center, scale_factor * pitch_radius);
+        doc->addCircle(&center, scale_factor);
     }
 
     if (drawAddendumCircleBox->isChecked()) {
         LAYER("addendums");
-        doc->addCircle(&center, scale_factor * addendum_radius);
+        doc->addCircle(&center, scale_factor * ev.addendum_radius);
     }
 
     if (drawRootCircleBox->isChecked()) {
         LAYER("dedendums");
-        doc->addCircle(&center, scale_factor * dedendum_radius);
+        doc->addCircle(&center, scale_factor * ev.dedendum_radius);
     }
 
     if (drawBaseCircleBox->isChecked()) {
         LAYER("base_lines");
-        doc->addCircle(&center, scale_factor * pitch_radius * cos_p_angle);
+        doc->addCircle(&center, scale_factor * ev.cos_p_angle);
     }
 
     if (drawPressureLineBox->isChecked() || drawPressureLimitBox->isChecked()) {
         LAYER("action_lines");
-        QPointF p1(scale_factor * cos(p_angle + rotation),
-                   scale_factor * sin(p_angle + rotation)),
-                p2(scale_factor * pitch_radius * cos(rotation),
-                   scale_factor * pitch_radius * sin(rotation));
+        QPointF p1(scale_factor * cos(ev.p_angle + rotation) * ev.cos_p_angle,
+                   scale_factor * sin(ev.p_angle + rotation) * ev.cos_p_angle),
+                p2(scale_factor * cos(rotation),
+                   scale_factor * sin(rotation));
         p1 += center; p2 += center;
         if (drawPressureLimitBox->isChecked())
             doc->addLine(&center, &p1);
@@ -454,6 +472,7 @@ void lc_Geardlg::processAction(Document_Interface *doc, const QString& cmd, QPoi
     if (useLayersBox->isChecked())
         doc->setLayer(lastLayer);
 
+    writeSettings();
 }
 
 void lc_Geardlg::checkAccept()
@@ -463,31 +482,11 @@ void lc_Geardlg::checkAccept()
 
 lc_Geardlg::~lc_Geardlg()
 {
-    INFO("deleting GEAR dialog\n");
-    //delete rotateBox;
-    //delete nteethBox;
-    //delete modulusBox;
-    //delete pressureBox;
-    //delete addendumBox;
-    //delete dedendumBox;
-    //delete n1Box;
-    //delete n2Box;
-    //delete useLayersBox;
-    //delete drawAddendumCircleBox;
-    //delete drawPitchCircleBox;
-    //delete drawBaseCircleBox;
-    //delete drawRootCircleBox;
-    //delete drawPressureLineBox;
-    //delete drawPressureLimitBox;
-    //delete calcInterferenceBox;
-    //delete n3Box;
-    INFO("deleted GEAR dialog\n");
 }
 
 void lc_Geardlg::closeEvent(QCloseEvent *event)
 {
     INFO("BEGIN\n");
-    writeSettings();
     QWidget::closeEvent(event);
     INFO("END\n");
 }
