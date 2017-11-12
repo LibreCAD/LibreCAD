2791cba2bee1c17eb2fc0ef39ecc5c86b66ab7b0
Simon Wells <simonrwells@gmail.com>
2017-11-12 15:40:04 +1300

    Make the postprocess-osx script more portable by using the QT_INSTALL_BINS path from qmake to find lrelease

--- scripts/postprocess-osx.sh
+++ scripts/postprocess-osx.sh
@@ -4,11 +4,7 @@ THISDIR="`pwd`"
 RESOURCEDIR="`pwd`/LibreCAD.app/Contents"
 TSDIRLC="`pwd`/librecad/ts"
 TSDIRPI="`pwd`/plugins/ts"
-LRELEASE="lrelease"
-if [ -z "$(which lrelease)" ] && [ -x "/opt/local/libexec/qt5/bin/lrelease" ]
-then
-	LRELEASE="/opt/local/libexec/qt5/bin/lrelease"
-fi
+LRELEASE="$1/lrelease"
 
 cd "$THISDIR"
 
@@ -19,20 +15,25 @@ mkdir -p $RESOURCEDIR/PlugIns
 cp librecad/support/patterns/*.dxf $RESOURCEDIR/Resources/patterns
 cp librecad/support/fonts/*.lff $RESOURCEDIR/Resources/fonts
 
-# Generate translations
-$LRELEASE librecad/src/src.pro
-$LRELEASE plugins/plugins.pro
-mkdir -p $RESOURCEDIR/Resources/qm
+if [ -x $LRELEASE ]
+then
+	# Generate translations
+	$LRELEASE librecad/src/src.pro
+	$LRELEASE plugins/plugins.pro
+	mkdir -p $RESOURCEDIR/Resources/qm
 
-# Go into translations directory
-cd "$TSDIRLC"
-for tf in *.qm
-do
-	cp $tf $RESOURCEDIR/Resources/qm/$tf
-done
+	# Go into translations directory
+	cd "$TSDIRLC"
+	for tf in *.qm
+	do
+		cp $tf $RESOURCEDIR/Resources/qm/$tf
+	done
 
-cd "$TSDIRPI"
-for tf in *.qm
-do
-	cp $tf $RESOURCEDIR/Resources/qm/$tf
-done
+	cd "$TSDIRPI"
+	for tf in *.qm
+	do
+		cp $tf $RESOURCEDIR/Resources/qm/$tf
+	done
+else
+	echo "WARNING: lrelease not found - Translations will not be generated"
+fi
