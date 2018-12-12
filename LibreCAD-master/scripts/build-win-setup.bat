3fbeb11add1c69413e216d54bd2dc17f4fb74a56
xcg <852390212@qq.com>
2018-12-12 11:56:50 +0800

    delete LibreCAD-master

--- LibreCAD-master/scripts/build-win-setup.bat
+++
@@ -1,8 +0,0 @@
-call set-windows-env.bat
-
-if _%LC_NSIS_FILE%==_ (
-    set LC_NSIS_FILE=nsis-5.4.nsi
-)
-pushd postprocess-windows
-makensis.exe /X"SetCompressor /FINAL lzma" /V4 %LC_NSIS_FILE%
-popd
