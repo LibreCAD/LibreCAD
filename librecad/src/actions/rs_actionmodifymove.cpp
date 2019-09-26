69a43e46206fb845ad8cd9a9cf117ecd93d0e779
kordik <kordik@psyche4.it.cas.cz>
2019-09-26 12:25:40 +0200

    When drawing a line by a mouse and holding the "Shift" key during placing the line endpoint, one can move the cursor only along certain angles (0°, 15°, 30°, 45°, etc.). This commit enables this feature also for moving and copying entities. I. e. when the reference point is selected and the entity is moved, by pressing the "Shift" key one can move the reference point (and the entity) only along the certaing angles (0°, 15°, 30°, 45°, etc.) measured from the original reference point position.

--- librecad/src/actions/rs_actionmodifymove.cpp
+++ librecad/src/actions/rs_actionmodifymove.cpp
@@ -24,9 +24,12 @@
 **
 **********************************************************************/
 
+
+
+#include<cmath>
+#include <QAction>
 #include "rs_actionmodifymove.h"
 
-#include <QAction>
 #include <QMouseEvent>
 #include "rs_dialogfactory.h"
 #include "rs_graphicview.h"
@@ -34,6 +37,18 @@
 #include "rs_modification.h"
 #include "rs_preview.h"
 #include "rs_debug.h"
+
+#include "rs_line.h"
+//#include "rs_pen.h"
+
+/*
+#include <QMouseEvent>
+#include "rs_actioneditundo.h"
+#include "rs_commands.h"
+#include "rs_commandevent.h"
+*/
+
+
 
 struct RS_ActionModifyMove::Points {
 	RS_MoveData data;
@@ -64,27 +79,68 @@ void RS_ActionModifyMove::trigger() {
 }
 
 
+RS_Vector RS_ActionModifyMove::snapToAngle(const RS_Vector &currentCoord)
+{
+    if(getStatus() != SetTargetPoint)
+    {
+        RS_DEBUG->print(RS_Debug::D_WARNING, "Trying to snap to angle when not setting EndPoint!");
+        return currentCoord;
+    }
+
+    if(snapMode.restriction != RS2::RestrictNothing || snapMode.snapGrid)
+    {
+        return currentCoord;
+    }
+    double angle = pPoints->referencePoint.angleTo(currentCoord)*180.0/M_PI;
+    /*Snapping to angle(15*) if shift key is pressed*/
+    const double angularResolution=15.;
+    angle -= remainder(angle,angularResolution);
+    angle *= M_PI/180.;
+    RS_Vector res = RS_Vector::polar(pPoints->referencePoint.distanceTo(currentCoord),angle);
+    res += pPoints->referencePoint;
+    snapPoint(res, true);
+    return res;
+}
+
 
 void RS_ActionModifyMove::mouseMoveEvent(QMouseEvent* e) {
     RS_DEBUG->print("RS_ActionModifyMove::mouseMoveEvent begin");
 
-    if (getStatus()==SetReferencePoint ||
-            getStatus()==SetTargetPoint) {
+    if (getStatus()==SetReferencePoint || getStatus()==SetTargetPoint)
+    {
 
         RS_Vector mouse = snapPoint(e);
-        switch (getStatus()) {
+        switch (getStatus())
+        {
         case SetReferencePoint:
 			pPoints->referencePoint = mouse;
             break;
 
         case SetTargetPoint:
-			if (pPoints->referencePoint.valid) {
+            if (pPoints->referencePoint.valid)
+            {
+                if(e->modifiers() & Qt::ShiftModifier)
+                    mouse = snapToAngle(mouse);
+
 				pPoints->targetPoint = mouse;
 
                 deletePreview();
                 preview->addSelectionFrom(*container);
 				preview->move(pPoints->targetPoint-pPoints->referencePoint);
+
+                if(e->modifiers() & Qt::ShiftModifier)
+                {
+                    auto line = new RS_Line(pPoints->referencePoint, mouse);
+                    preview->addEntity(line);
+                    RS_Vector factor(2,2);
+                    line->scale(line->getMiddlePoint(), factor);
+                    line->setLayerToActive();
+                    line->setPenToActive();
+
+                }
                 drawPreview();
+
+
             }
             break;
 
@@ -100,13 +156,23 @@ void RS_ActionModifyMove::mouseMoveEvent(QMouseEvent* e) {
 
 void RS_ActionModifyMove::mouseReleaseEvent(QMouseEvent* e) {
     if (e->button()==Qt::LeftButton) {
-        RS_CoordinateEvent ce(snapPoint(e));
+        RS_Vector snapped = snapPoint(e);
+
+
+        if((e->modifiers() & Qt::ShiftModifier) && getStatus() == SetTargetPoint )
+            snapped = snapToAngle(snapped);
+
+
+        RS_CoordinateEvent ce(snapped);
         coordinateEvent(&ce);
     } else if (e->button()==Qt::RightButton) {
         deletePreview();
         init(getStatus()-1);
     }
 }
+
+
+
 
 void RS_ActionModifyMove::coordinateEvent(RS_CoordinateEvent* e) {
 
