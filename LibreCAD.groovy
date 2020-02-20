fea8f55b10889e9226f6e07c924dc33e971f64f0
Michael Nitsche <Michael.Nitsche@hypertherm.com>
2020-02-20 16:45:17 -0500

    Updated quotes for snippet.

--- LibreCAD.groovy
+++ LibreCAD.groovy
@@ -7,7 +7,7 @@ def Build()
 	def
 		existingPath = env.PATH;
 	def cmdText = '"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxiliary\\Build\\vcvars32.bat" && ' +
-					'"' + "SET PATH="%PATH%;${existingPath}" + '" && ' +
+					'"' + 'SET PATH="%PATH%;${existingPath}"' + '" && ' +
 					'"qmake libreCAD.pro -tp vc -r" &&' +
 					'"msbuild librecad.sln  /p:Platform=Win32 /p:Configuration=Release"'
 	bat "${cmdText}"
