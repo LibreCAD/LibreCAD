391dec11f76e1840728cf750b474287b99e42d07
Michael Nitsche <Michael.Nitsche@hypertherm.com>
2020-02-20 15:44:19 -0500

    Nested quotes in order to get the command to run correctly.

--- LibreCAD.groovy
+++ LibreCAD.groovy
@@ -4,7 +4,7 @@ def LibreCadExePath()
 }
 def Build()
 {
-	bat "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxiliary\\Build\\vcvars32.bat"
+	bat '"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxiliary\\Build\\vcvars32.bat"'
 	bat "qmake libreCAD.pro -tp vc -r"
 	bat "msbuild librecad.sln  /p:Platform=Win32 /p:Configuration=Release"
 }
