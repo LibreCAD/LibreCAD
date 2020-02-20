79c502207ca0581908323c044102c337d5083099
Michael Nitsche <Michael.Nitsche@hypertherm.com>
2020-02-20 16:57:48 -0500

    added /k to keep that session open.

--- LibreCAD.groovy
+++ LibreCAD.groovy
@@ -6,7 +6,7 @@ def Build()
 {
 	def
 		existingPath = env.PATH;
-	def cmdText = '"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxiliary\\Build\\vcvars32.bat" && ' +
+	def cmdText = 'cmd /k "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxiliary\\Build\\vcvars32.bat" && ' +
 					'SET PATH="%PATH%;' + existingPath + '" && ' +
 					'"qmake libreCAD.pro -tp vc -r" &&' +
 					'"msbuild librecad.sln  /p:Platform=Win32 /p:Configuration=Release"'
