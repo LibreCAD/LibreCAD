cf11efd297baae6c435aa0d22010297c72cbc95a
Michael Nitsche <Michael.Nitsche@hypertherm.com>
2020-02-21 11:07:24 -0500

    Updated jenkins script to only build and stash the exes whereever they are.

--- LibreCAD.groovy
+++ LibreCAD.groovy
@@ -6,22 +6,7 @@ def Build()
 {
 	bat "%WORKSPACE%\\BuildLibreCAD.bat"
 }
-def GetBuiltFiles()
-{
-	File myObj = new File("$WORKSPACE\\windows\\"); 
-	String[] files = myObj.list();
-	String fileNames = ""
-	int size = files.length;
-	for (int i=0; i<size; i++)
-	{
-		fileNames = fileNames + ","
-	}
-	
-	if(fileNames.isEmpty())
-		return fileNames
-	
-	return fileNames.substring(0, fileNames.length()-1)
-}
+
 def BuildInstaller()
 {
 	bat "mkdir %WORKSPACE%\\generated\\"
