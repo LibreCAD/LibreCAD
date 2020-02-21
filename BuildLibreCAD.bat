3a86bc34f8716159b248a0109c5b7935acd0b46d
Michael Nitsche <Michael.Nitsche@hypertherm.com>
2020-02-21 10:29:53 -0500

    Edited syntax of batch file.

--- BuildLibreCAD.bat
+++ BuildLibreCAD.bat
@@ -1,3 +1,3 @@
-"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxiliary\\Build\\vcvars32.bat"
-"qmake libreCAD.pro -tp vc -r"
-"msbuild librecad.sln  /p:Platform=Win32 /p:Configuration=Release"
+call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxiliary\\Build\\vcvars32.bat"
+call "qmake libreCAD.pro -tp vc -r"
+call "msbuild librecad.sln  /p:Platform=Win32 /p:Configuration=Release"
