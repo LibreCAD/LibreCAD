d20ee42c647392d1698893dd5b99ff2494526875
Michael Nitsche <Michael.Nitsche@hypertherm.com>
2020-02-21 10:37:57 -0500

    Updated batch file.

--- BuildLibreCAD.bat
+++ BuildLibreCAD.bat
@@ -1,3 +1,3 @@
-call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxiliary\\Build\\vcvars32.bat"
+call %comspec% /k "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxiliary\\Build\\vcvars32.bat"
 call "qmake libreCAD.pro -tp vc -r"
 call "msbuild librecad.sln  /p:Platform=Win32 /p:Configuration=Release"
