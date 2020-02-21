eac54436b081688c30fac6658a8983e7b212aa4b
Michael Nitsche <Michael.Nitsche@hypertherm.com>
2020-02-21 10:20:12 -0500

    Doing a test.

--- LibreCAD.groovy
+++ LibreCAD.groovy
@@ -7,7 +7,7 @@ def Build()
 	def cmdText = '"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\Auxiliary\\Build\\vcvars32.bat" && ' +
 					'"qmake libreCAD.pro -tp vc -r" &&' +
 					'"msbuild librecad.sln  /p:Platform=Win32 /p:Configuration=Release"'
-	bat "${cmdText}"
+	bat "qmake libreCAD.pro -tp vc -r"
 }
 def GetBuiltFiles()
 {
