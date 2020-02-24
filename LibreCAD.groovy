19999460ef9ad58b42d10f806068c2bfef0d313f
Michael Nitsche <Michael.Nitsche@hypertherm.com>
2020-02-24 15:38:44 -0500

    Updated logic to copy in file necessities from network.

--- LibreCAD.groovy
+++ LibreCAD.groovy
@@ -6,9 +6,21 @@ def Build()
 {
 	bat "%WORKSPACE%\\BuildLibreCAD.bat"
 }
-
+def CopyProNestFileToWindowsFolder(fileName)
+{
+	def proNestVersion = 13.1.4
+	def proNestFileDirectory = "\\\\cam-file\\devcommon\\Jenkins\\LibreCad\\Built by ProNest\\" + proNestVersionb + "\\"
+	bat 'copy "' + proNestFileDirectory + fileName + '" ' + '"%WORKSPACE%\\windows\\"'
+}
+def PreBuild()
+{
+	CopyProNestFileToWindowsFolder("DwgReader.dll")
+	CopyProNestFileToWindowsFolder("Microsoft.ApplicationInsights.dll")
+	CopyProNestFileToWindowsFolder("Telemetry.dll")
+}
 def BuildInstaller()
 {
+	PreBuild()
 	bat "cd scripts && %WORKSPACE%\\scripts\\build-windows.bat"
 }
 def GetInstallerPath()
