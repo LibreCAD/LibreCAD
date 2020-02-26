a2b4bd0f6f18e4ffa2646642be9d05ba0e3b2b97
Michael Nitsche <Michael.Nitsche@hypertherm.com>
2020-02-26 11:56:20 -0500

    Updated pre-build to copy in necessary files from binary management.

--- LibreCAD.groovy
+++ LibreCAD.groovy
@@ -17,12 +17,21 @@ def PreBuild()
 	CopyProNestFileToWindowsFolder("DwgReader.dll")
 	CopyProNestFileToWindowsFolder("Microsoft.ApplicationInsights.dll")
 	CopyProNestFileToWindowsFolder("Telemetry.dll")
+	CopyBinaryManagementFile("Win32\\SHP2LFF\\S2FDLL.dll")
+	CopyBinaryManagementFile("Win32\\SHP2LFF\\shp2lff.exe")
+}
+def CopyBinaryManagementFile(pathInBinaryManagement)
+{
+	def
+		binaryManagementFolder = "\\\\cam-file\\Devcommon\\Jenkins\\Binary Management\\"
+	def 
+		filePath = binaryManagementFolder + pathInBinaryManagement
+	bat 'copy "' + filePath + '" ' + '"%WORKSPACE%\\windows\\"'
 }
 def BuildInstaller()
 {
 	PreBuild()
 	bat "%WORKSPACE%\\BuildLibreCADInstaller.bat"
-	
 }
 def GetInstallerPath()
 {
