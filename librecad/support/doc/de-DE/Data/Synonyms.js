bcdb0051ac3e25778e4c02bd18d06ec52ef0bed6
angelia.giannone <Angelia.Giannone@hypertherm.com>
2020-04-09 15:16:50 -0400

    German (de-DE) Help support.

---
+++ librecad/support/doc/de-DE/Data/Synonyms.js
@@ -0,0 +1,8 @@
+var xmlSynonymsData = "";
+xmlSynonymsData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlSynonymsData += '<MadCapSynonyms xml:lang=\"de\">';
+xmlSynonymsData += '    <!-- saved from url=(0016)http://localhost -->';
+xmlSynonymsData += '    <Groups />';
+xmlSynonymsData += '    <Directional />';
+xmlSynonymsData += '</MadCapSynonyms>';
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Synonyms', xmlSynonymsData);
---
+++ librecad/support/doc/de-DE/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=637220417178969110'
+});
