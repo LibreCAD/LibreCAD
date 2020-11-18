3a45372b595a6f72f6a813cebf8df2413470aba4
jalovsiko <letovnn@gmail.com>
2020-11-18 13:08:09 -0500

    Added a basic parser, it still requires dependencies to be set

--- librecad/src/main/console_dxf2png.cpp
+++ librecad/src/main/console_dxf2png.cpp
@@ -1,8 +1,75 @@
+/******************************************************************************
+**
+** This file was created for the LibreCAD project, a 2D CAD program.
+**
+** Copyright (C) 2020 Nikita Letov <letovnn@gmail.com>
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file gpl-2.0.txt included in the
+** packaging of this file.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+**
+** This copyright notice MUST APPEAR in all copies of the script!
+**
+******************************************************************************/
+
+
+#include <QtCore>
+#include <QCoreApplication>
+#include <QApplication>
+
+#include "rs_debug.h"
+#include "rs_fontlist.h"
+#include "rs_patternlist.h"
+#include "rs_settings.h"
+#include "rs_system.h"
+
 #include "main.h"
 
 #include "console_dxf2pdf.h"
 
 int console_dxf2png(int argc, char* argv[])
 {
-    return 0;
+    RS_DEBUG->setLevel(RS_Debug::D_NOTHING);
+
+    QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("LibreCAD");
+    QCoreApplication::setApplicationName("LibreCAD");
+    QCoreApplication::setApplicationVersion(XSTR(LC_VERSION));
+
+    QFileInfo prgInfo(QFile::decodeName(argv[0]));
+    QString prgDir(prgInfo.absolutePath());
+    RS_SETTINGS->init(app.organizationName(), app.applicationName());
+    RS_SYSTEM->init(app.applicationName(), app.applicationVersion(),
+        XSTR(QC_APPDIR), prgDir);
+
+    QCommandLineParser parser;
+
+    QString appDesc;
+    QString librecad;
+    if (prgInfo.baseName() != "dxf2png") {
+        librecad = prgInfo.filePath();
+        appDesc = "\ndxf2png usage: " + prgInfo.filePath()
+            + " dxf2png [options] <dxf_files>\n";
+    }
+    appDesc += "\nPrint a DXF file to a PNG file.";
+    appDesc += "\n\n";
+    appDesc += "Examples:\n\n";
+    appDesc += "  " + librecad + " dxf2png *.dxf";
+    appDesc += "    -- print all dxf files to pdf files with the same names.\n";
+    appDesc += "\n";
+    appDesc += "  " + librecad + " dxf2pdf -o some.pdf *.dxf";
+    appDesc += "    -- print all dxf files to 'some.pdf' file.";
+    parser.setApplicationDescription(appDesc);
+
+    return app.exec();
 }
