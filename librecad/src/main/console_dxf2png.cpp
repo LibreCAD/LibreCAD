6099b98a614bca8580ac176f56b94a2f1127b1a5
jalovsiko <letovnn@gmail.com>
2020-11-18 12:01:09 -0500

    Added a template for the conversion to png based on the pdf converter presend

---
+++ librecad/src/main/console_dxf2png.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+
+#include "console_dxf2pdf.h"
+
+int console_dxf2png(int argc, char* argv[])
+{
+    return 0;
+}
---
+++ librecad/src/main/console_dxf2png.h
@@ -0,0 +1,11 @@
+/******************************************************************************
+**
+** Copyright (C) 2018 Nikita Letov <letovnn@gmail.com>
+**
+******************************************************************************/
+#ifndef CONSOLE_DXF2PNG_H
+#define CONSOLE_DXF2PNG_H
+
+int console_dxf2png(int argc, char** argv);
+
+#endif // CONSOLE_DXF2PNG_H
