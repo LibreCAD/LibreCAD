3490d41990b915edbf61a1766407cf1feb5887c1
vuphin <78334496+vuphin@users.noreply.github.com>
2021-02-07 15:11:15 +0700

    Update rs_graphicview.cpp

--- librecad/src/lib/gui/rs_graphicview.cpp
+++ librecad/src/lib/gui/rs_graphicview.cpp
@@ -1374,8 +1374,19 @@ void RS_GraphicView::drawRelativeZero(RS_Painter *painter) {
     painter->drawLine(RS_Vector(vp.x, vp.y-zr),
                       RS_Vector(vp.x, vp.y+zr)
                       );
-
-    painter->drawCircle(vp, 5);
+    painter->drawLine(RS_Vector(vp.x-zr, vp.y+zr),
+                    RS_Vector(vp.x+zr, vp.y+zr)
+                      );
+    painter->drawLine(RS_Vector(vp.x-zr, vp.y-zr),
+                    RS_Vector(vp.x+zr, vp.y-zr)
+                      );
+    painter->drawLine(RS_Vector(vp.x-zr, vp.y-zr),
+                      RS_Vector(vp.x-zr, vp.y+zr)
+                      );
+    painter->drawLine(RS_Vector(vp.x+zr, vp.y-zr),
+                      RS_Vector(vp.x+zr, vp.y+zr)
+                      );
+    //painter->drawCircle(vp, 5);
 }
 
 
