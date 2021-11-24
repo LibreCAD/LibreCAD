acd1260521d25bab777f20a899008a2eeb2b8b3f
Melwyn Francis Carlo <melwyncarlo@gmail.com>
2021-11-24 14:36:04 +0530

    Solved issue #1326

--- librecad/src/lib/engine/rs_entity.cpp
+++ librecad/src/lib/engine/rs_entity.cpp
@@ -851,6 +851,11 @@ RS_Pen RS_Entity::getPen(bool resolve) const {
             //}
         }
 
+        if (p.getWidth() == RS2::WidthDefault)
+        {
+            p.setWidth(RS2::Width07);
+        }
+
         return p;
     }
 }
