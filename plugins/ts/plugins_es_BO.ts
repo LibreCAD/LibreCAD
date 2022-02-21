11e95aab15ff224b97d03b3c0c4054c5807fe89d
Armin Stebich <github@lordofbikes.de>
2022-02-21 19:47:47 +0000

    New translations plugins_en.ts (Spanish, Bolivia)

--- plugins/ts/plugins_es_BO.ts
+++ plugins/ts/plugins_es_BO.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="es-BO" sourcelanguage="en">
+<TS version="2.0" language="es_BO" sourcelanguage="en">
   <context>
     <name>AsciiFile</name>
     <message>
