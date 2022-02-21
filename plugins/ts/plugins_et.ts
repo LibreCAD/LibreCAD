a9c2503bf9093cbfee67f619e12c982ed7872513
Armin Stebich <github@lordofbikes.de>
2022-02-21 19:47:36 +0000

    New translations plugins_en.ts (Estonian)

--- plugins/ts/plugins_et.ts
+++ plugins/ts/plugins_et.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="et" sourcelanguage="en">
+<TS version="2.0" language="et_EE" sourcelanguage="en">
   <context>
     <name>AsciiFile</name>
     <message>
