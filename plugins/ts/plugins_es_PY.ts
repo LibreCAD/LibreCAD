9794b029c74f4333a8224c2273bb4f6288edfcbb
Armin Stebich <github@lordofbikes.de>
2022-02-21 19:48:23 +0000

    New translations plugins_en.ts (Spanish, Paraguay)

--- plugins/ts/plugins_es_PY.ts
+++ plugins/ts/plugins_es_PY.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="es-PY" sourcelanguage="en">
+<TS version="2.0" language="es_PY" sourcelanguage="en">
   <context>
     <name>AsciiFile</name>
     <message>
