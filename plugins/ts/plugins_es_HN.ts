9cf189e4136de6a211eae3f7b2aa763e851c4836
Armin Stebich <github@lordofbikes.de>
2022-02-21 19:48:19 +0000

    New translations plugins_en.ts (Spanish, Honduras)

--- plugins/ts/plugins_es_HN.ts
+++ plugins/ts/plugins_es_HN.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="es-HN" sourcelanguage="en">
+<TS version="2.0" language="es_HN" sourcelanguage="en">
   <context>
     <name>AsciiFile</name>
     <message>
