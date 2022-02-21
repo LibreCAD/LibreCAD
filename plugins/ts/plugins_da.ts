1d858e8577546f9c06a493fe87c4ce7bac0460c0
Armin Stebich <github@lordofbikes.de>
2022-02-21 19:47:12 +0000

    New translations plugins_en.ts (Danish)

--- plugins/ts/plugins_da.ts
+++ plugins/ts/plugins_da.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="da" sourcelanguage="en">
+<TS version="2.0" language="da_DK" sourcelanguage="en">
   <context>
     <name>AsciiFile</name>
     <message>
