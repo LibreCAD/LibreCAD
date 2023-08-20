05153a2dc42357b1054afedd0bd9fe4d0f47a862
Dongxu Li <dongxuli2011@gmail.com>
2023-08-20 04:28:16 -0400

    LoopUtils:: add license

--- librecad/src/lib/engine/lc_looputils.cpp
+++ librecad/src/lib/engine/lc_looputils.cpp
@@ -1,3 +1,27 @@
+/*
+**********************************************************************************
+**
+** This file was created for the LibreCAD project (librecad.org), a 2D CAD program.
+**
+** Copyright (C) 2023 librecad (www.librecad.org)
+** Copyright (C) 2023 dxli (github.com/dxli)
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+**********************************************************************************
+*/
 #include <algorithm>
 #include <array>
 #include <map>
