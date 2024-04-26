f974ebe32e0da575641c77449455e187e3d825d3
Dongxu Li <dongxuli2011@gmail.com>
2024-04-26 07:04:41 -0400

    Context menu: fixed Text editing

--- librecad/src/lib/engine/rs_block.cpp
+++ librecad/src/lib/engine/rs_block.cpp
@@ -68,22 +68,14 @@ RS_Entity* RS_Block::clone() const {
 
 RS_LayerList* RS_Block::getLayerList() {
     RS_Graphic* g = getGraphic();
-    if (g) {
-        return g->getLayerList();
-    } else {
-        return NULL;
-    }
+    return (g != nullptr) ? g->getLayerList() : nullptr;
 }
 
 
 
 RS_BlockList* RS_Block::getBlockList() {
     RS_Graphic* g = getGraphic();
-    if (g) {
-        return g->getBlockList();
-    } else {
-        return NULL;
-    }
+    return (g != nullptr) ? g->getBlockList() : nullptr;
 }
 
 
