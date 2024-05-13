acac693d4ef7034eff7381e20c75cadc9dc91134
Dongxu Li <dongxuli2011@gmail.com>
2024-05-13 05:15:24 -0400

    Issue #1769: installer naming fixes

--- CI/build-appimg.sh
+++ CI/build-appimg.sh
@@ -70,9 +70,9 @@ find appdir/
 wget -c https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous -O - | grep "appimagetool-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
 chmod +x appimagetool-*.AppImage
 ./appimagetool-*.AppImage deploy appdir/usr/share/applications/librecad.desktop
-VERSION=`git describe --tags --always` ./appimagetool-*.AppImage appdir/
+VERSION=`git describe --always` ./appimagetool-*.AppImage appdir/
 #wget -q https://github.com/omergoktas/linuxdeployqt/releases/download/latest/linuxdeployqt-x86_64.AppImage
 #chmod +x linuxdeployqt-x86_64.AppImage
 #./linuxdeployqt-x86_64.AppImage appdir/usr/bin/librecad -qmake=$(which qmake6) -appimage
 #./appimagetool-*.AppImage -s deploy appdir/usr/bin/librecad.desktop
-#VERSION=`git describe --tags --always` ./appimagetool-*.AppImage appdir/
+#VERSION=`git describe --always` ./appimagetool-*.AppImage appdir/
