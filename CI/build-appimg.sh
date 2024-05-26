006962e7094dce8aa050b408516d89027254bf0d
Dongxu Li <dongxuli2011@gmail.com>
2024-05-26 17:10:55 -0400

    Update build-appimg.sh

--- CI/build-appimg.sh
+++ CI/build-appimg.sh
@@ -73,6 +73,6 @@ wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/lin
 wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
 chmod +x *.AppImage
 #ARCH=x86_64 ./appimagetool-*.AppImage deploy appdir/usr/share/applications/librecad.desktop
-ARCH=x86_64 ./linuxdeploy-x86_64.AppImage--appdir appdir -e appdir/usr/bin/librecad -d appdir/usr/share/applications/librecad.desktop
+ARCH=x86_64 ./linuxdeploy-x86_64.AppImage --appdir appdir -e appdir/usr/bin/librecad -d appdir/usr/share/applications/librecad.desktop
 ARCH=x86_64 ./linuxdeploy-plugin-qt-x86_64.AppImage --appdir appdir
 VERSION=`git describe --always` ARCH=x86_64 ./appimagetool-*.AppImage appdir/
