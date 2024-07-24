1f567e1d0cdf6735988e10377cee6d76dcd1c8f1
sand1024 <sand1024@gmail.com>
2024-07-24 20:14:12 +0300

    additional modifiers wrapper macros

--- librecad/src/lib/actions/lc_modifiersinfo.h
+++ librecad/src/lib/actions/lc_modifiersinfo.h
@@ -4,6 +4,10 @@
 #include <QInternal>
 
 #define MOD_NONE LC_ModifiersInfo::NONE()
+#define MOD_SHIFT(msg) LC_ModifiersInfo::SHIFT(msg)
+#define MOD_CTRL(msg) LC_ModifiersInfo::CTRL(msg)
+#define MOD_SHIFT_AND_CTRL(shiftMsg, ctrlMsg) LC_ModifiersInfo::SHIFT_AND_CTRL(shiftMsg,ctrlMsg)
+#define MOD_SHIFT_AND_CTRL_ANGLE(ctrlMsg) LC_ModifiersInfo::SHIFT_AND_CTRL(LC_ModifiersInfo::MSG_ANGLE_SNAP,ctrlMsg)
 #define MOD_SHIFT_ANGLE_SNAP LC_ModifiersInfo::SHIFT_ANGLE_SNAP()
 #define MOD_SHIFT_FREE_SNAP LC_ModifiersInfo::SHIFT_FREE_SNAP()
 #define MOD_SHIFT_MIRROR_ANGLE LC_ModifiersInfo::SHIFT_MIRROR_ANGLE()
