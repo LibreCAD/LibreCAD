e82442df7d1377d8744e7a0c844a0d642197ab46
Dongxu Li <dongxuli2011@gmail.com>
2024-08-31 22:49:56 -0400

    Issue #1874: command line update logic

--- librecad/src/ui/dialogs/qg_dialogfactory.cpp
+++ librecad/src/ui/dialogs/qg_dialogfactory.cpp
@@ -1103,9 +1103,11 @@ void QG_DialogFactory::updateMouseWidget(const QString& left,
     if (mouseWidget) {
         mouseWidget->setHelp(left, right, modifiers);
     }
-    if (commandWidget) {
-        commandWidget->setCommand(left);
-    }
+    // Issue #1874: Review the following logic. If confirmed as a mistake, delete this commented out
+    // section
+    // if (commandWidget) {
+    //     commandWidget->setCommand(left);
+    // }
 }
 
 /**
