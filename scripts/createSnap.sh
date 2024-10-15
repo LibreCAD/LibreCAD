cd7bd7b92696331fff60709c6c98d80ca4131377
Dongxu Li <dongxuli2011@gmail.com>
2024-10-14 23:27:56 -0400

    Issue #1891: snap support

--- scripts/createSnap.sh
+++ scripts/createSnap.sh
@@ -3,5 +3,5 @@ sudo snap install snapcraft --classic
 sudo snap run snapcraft --destructive-mode
  
 sudo snap remove librecad --purge
-sudo snap install librecad_2.2.1-beta_amd64.snap --jailmode
+sudo snap install --dangerous librecad_2.2.1-rc_amd64.snap --jailmode
 
