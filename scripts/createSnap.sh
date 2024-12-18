0e5fc4d469af7579463c451d37accd00d4eeccbb
Dongxu Li <dongxuli2011@gmail.com>
2024-12-17 19:07:01 -0500

    snapcraft: initia support for Qt6

--- scripts/createSnap.sh
+++ scripts/createSnap.sh
@@ -1,7 +1,4 @@
 sudo snap install snapcraft --classic
-
 sudo snap run snapcraft --destructive-mode
  
-sudo snap remove librecad --purge
-sudo snap install librecad_2.2.1-beta_amd64.snap --jailmode
 
