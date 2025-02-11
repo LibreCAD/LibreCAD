a5e47b505c259858453b0e0e74d7d90016992d79
Dongxu Li <dongxuli2011@gmail.com>
2025-02-11 13:09:18 -0500

    Issue #2030: avoid zero size arrows
    The default precision=6 may cause issues

--- librecad/src/lib/engine/rs_dimension.cpp
+++ librecad/src/lib/engine/rs_dimension.cpp
@@ -462,12 +462,11 @@ void RS_Dimension::updateCreateAlignedTextDimensionLine(const RS_Vector& p1,
     double distance = p1.distanceTo(p2);
     // arrow size:
     double arrowSize = getArrowSize()*dimscale;
+    const bool showArrow = arrowSize >= 1e-6 * p1.distanceTo(p2);
 
     // do we have to put the arrows outside of the line?
     bool outsideArrows = (distance<arrowSize*2.5);
 
-    // arrow angles:
-    double arrowAngle1, arrowAngle2;
 
     RS_Pen pen(getDimensionLineColor(),
            getDimensionLineWidth(),
@@ -480,8 +479,8 @@ void RS_Dimension::updateCreateAlignedTextDimensionLine(const RS_Vector& p1,
     addEntity(dimensionLine);
 
     // Text label:
-    RS_MTextData textData;
-    RS_Vector textPos;
+    RS_MTextData textData{};
+    RS_Vector textPos{};
     double dimAngle1 = dimensionLine->getAngle1();
     bool corrected = false;
     double textAngle = RS_Math::makeAngleReadable(dimAngle1, true, &corrected);
@@ -520,9 +519,8 @@ void RS_Dimension::updateCreateAlignedTextDimensionLine(const RS_Vector& p1,
     text->setLayer(nullptr);
 
     // move text to the side:
-    RS_Vector distH;
     if (text->getUsedTextWidth()>distance) {
-        distH.setPolar(text->getUsedTextWidth()/2.0
+        auto distH = RS_Vector::polar(text->getUsedTextWidth()/2.0
                        +distance/2.0+dimgap, textAngle);
         text->move(distH);
     }
@@ -530,12 +528,10 @@ void RS_Dimension::updateCreateAlignedTextDimensionLine(const RS_Vector& p1,
     addEntity(text);
 
     // add arrows
-    if (outsideArrows==false) {
-        arrowAngle1 = dimensionLine->getAngle2();
-        arrowAngle2 = dimensionLine->getAngle1();
-    } else {
-        arrowAngle1 = dimensionLine->getAngle1();
-        arrowAngle2 = dimensionLine->getAngle2();
+    // arrow angles:
+    double arrowAngle1 = outsideArrows ? dimensionLine->getAngle1() : dimensionLine->getAngle2();
+    double arrowAngle2 = outsideArrows ? dimensionLine->getAngle2() : dimensionLine->getAngle1();
+    if (!outsideArrows && showArrow)  {
 
         // extend dimension line outside arrows
         RS_Vector dir = RS_Vector::polar(arrowSize*2, arrowAngle2);
@@ -546,12 +542,11 @@ void RS_Dimension::updateCreateAlignedTextDimensionLine(const RS_Vector& p1,
     if(dimtsz < 0.01) {
         //display arrow
         // Arrows:
-        RS_SolidData sd;
-        RS_Solid* arrow;
+        RS_SolidData sd{};
 
-        if (arrow1) {
+        if (arrow1 && showArrow) {
             // arrow 1
-            arrow = new RS_Solid(this, sd);
+            auto arrow = new RS_Solid(this, sd);
             arrow->shapeArrow(p1,
                               arrowAngle1,
                               arrowSize);
@@ -560,9 +555,9 @@ void RS_Dimension::updateCreateAlignedTextDimensionLine(const RS_Vector& p1,
             addEntity(arrow);
         }
 
-        if (arrow2) {
+        if (arrow2 && showArrow) {
             // arrow 2:
-            arrow = new RS_Solid(this, sd);
+            auto arrow = new RS_Solid(this, sd);
             arrow->shapeArrow(p2,
                               arrowAngle2,
                               arrowSize);
@@ -574,12 +569,11 @@ void RS_Dimension::updateCreateAlignedTextDimensionLine(const RS_Vector& p1,
         //display ticks
         // Arrows:
 
-        RS_Line* tick;
         RS_Vector tickVector = RS_Vector::polar(dimtsz,arrowAngle1 + M_PI*0.25); //tick is 45 degree away
 
         if (arrow1) {
             // tick 1
-            tick = new RS_Line(this, p1-tickVector, p1+tickVector);
+            auto tick = new RS_Line(this, p1-tickVector, p1+tickVector);
             tick->setPen(pen);
             tick->setLayer(nullptr);
             addEntity(tick);
@@ -587,7 +581,7 @@ void RS_Dimension::updateCreateAlignedTextDimensionLine(const RS_Vector& p1,
 
         if (arrow2) {
             // tick 2:
-            tick = new RS_Line(this, p2-tickVector, p2+tickVector);
+            auto tick = new RS_Line(this, p2-tickVector, p2+tickVector);
             tick->setPen(pen);
             tick->setLayer(nullptr);
             addEntity(tick);
