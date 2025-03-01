ad78339adf3b3f2faa9eb7ded3fce7ad2930ac91
Emanuel Strobel <emanuel.strobel@gmail.com>
2025-03-01 23:55:10 +0100

    revert from developer

--- librecad/src/actions/drawing/rs_actiondefault.cpp
+++ librecad/src/actions/drawing/rs_actiondefault.cpp
@@ -249,15 +249,11 @@ void RS_ActionDefault::mouseMoveEvent(QMouseEvent *e){
         case Neutral: {
             deleteSnapper();
             highlightHoveredEntities(e);
-            if (infoCursorOverlayPrefs && infoCursorOverlayPrefs->enabled){
+            if (infoCursorOverlayPrefs->enabled){
                 if (!isShowEntityDescriptionOnHighlight()) {
                     infoCursorOverlayData.setZone2("");
                 }
                 RS_Snapper::forceUpdateInfoCursor(mouse);
-            }
-            else
-            {
-                qCritical() << "[RS_ActionDefault::mouseMoveEvent] ###FIXME infoCursorOverlayPrefs == NULL!!";
             }
             break;
         }
