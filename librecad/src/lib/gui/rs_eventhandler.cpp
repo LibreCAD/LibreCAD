095c6a6dc148ef59df8651a8896728e87f00997b
sand1024 <sand1024@gmail.com>
2025-05-21 10:32:47 +0300

    fix for LibreCAD#2153

--- librecad/src/lib/gui/rs_eventhandler.cpp
+++ librecad/src/lib/gui/rs_eventhandler.cpp
@@ -65,11 +65,15 @@ RS_EventHandler::~RS_EventHandler() {
 }
 
 void RS_EventHandler::uncheckQAction(){
-    if (!hasAction()){
-        if (m_QAction != nullptr) {
-            m_QAction->setChecked(false);
-            m_QAction = nullptr;
-        }
+    if (m_QAction != nullptr) {
+        m_QAction->setChecked(false);
+        m_QAction = nullptr;
+    }
+    if (hasAction()){
+        auto lastAction = m_currentActions.last();
+        m_graphicView->notifyActiveAction(lastAction.get()); // ugly fix to properly restore previous action icon etc.
+    }
+    else {
         m_graphicView->notifyNoActiveAction();
     }
 }
@@ -447,8 +451,9 @@ void RS_EventHandler::cleanUp() {
     RS_DEBUG->print("RS_EventHandler::cleanUp");
 
     if (hasAction()) {
-        m_currentActions.last()->resume();
-        m_currentActions.last()->showOptions();
+        auto lastAction = m_currentActions.last();
+        lastAction->resume();
+        lastAction->showOptions();
     } else {
         if (m_defaultAction) {
             m_defaultAction->resume();
@@ -509,7 +514,16 @@ void RS_EventHandler::setQAction(QAction *action) {
 //    LC_ERR << __func__ << "()";
     debugActions();
     if (m_QAction != nullptr && m_QAction != action) {
-        killAllActions();
+        auto property = action->property("RS2:actionType");
+        if (property.isValid()) {
+            int rtti = property.toInt();
+             if (rtti != RS2::ActionZoomPan && rtti != RS2::ActionSetRelativeZero) {
+                 killAllActions();
+             }
+        }
+        else {
+            killAllActions();
+        }
     }
     m_QAction = action;
 }
