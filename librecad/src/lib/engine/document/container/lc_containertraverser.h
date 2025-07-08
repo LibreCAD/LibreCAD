84a2ad215d3ef4d88286af095da41b5fda136f76
dongxuli-concord-design <102729580+dongxuli-concord-design@users.noreply.github.com>
2025-07-07 21:51:46 -0400

    Comments

--- librecad/src/lib/engine/document/container/lc_containertraverser.h
+++ librecad/src/lib/engine/document/container/lc_containertraverser.h
@@ -28,20 +28,41 @@
 #include <memory>
 #include <vector>
 
-class RS_AtomicEntity;
 class RS_Entity;
 class RS_EntityContainer;
-class RS_Line;
-class RS_Vector;
-class RS_VectorSolutions;
 
 namespace RS2 {
     enum ResolveLevel: unsigned short;
 }
 namespace lc {
 
+/**
+ * @brief The LC_ContainerTraverser class
+ *        This is an implementation of RS_EntityContainer traversing methods to
+ *        avoid using member attributes of the container.
+ *        Use the traverser instead of RS_EntityContainer methods:
+ *   RS_Entity* firstEntity(RS2::ResolveLevel) const;
+ *   RS_Entity* lastEntity(RS2::ResolveLevel) const;
+ *   RS_Entity* nextEntity(RS2::ResolveLevel) const;
+ *   RS_Entity* prevEntity(RS2::ResolveLevel) const;
+ *
+ *   The deprecated way to traverse a container:
+ *     for(auto* entity=container.firstEntity(level); entity; entity=container.nextEntity());
+ *
+ *   The recommended way:
+ *     LC_ContainerTraverser traverser{container, level};
+ *     for(auto* entity=traverser.first(); entity; entity=traverser.next());
+ *   or,
+ *     for(auto* entity: LC_ContainerTraverser{container, level}.entities());
+ *   The difference between those two new methods is whether to generate a list
+ *   of container entities before looping through them. The difference is only
+ *   important, if traverse must be customized based on the current entity.
+ * @author Dongxu Li
+ */
 class LC_ContainerTraverser {
 public:
+
+    // The direction of traversing
     enum class Direction: short
     {
         Forward = 0,
@@ -52,27 +73,42 @@ public:
                           LC_ContainerTraverser::Direction direction = Direction::Forward);
     ~LC_ContainerTraverser();
 
+    /**
+     * @brief first() - intended to replace RS_Container::firstEntity()
+     * @return - the first entity in the container
+     */
     RS_Entity* first();
+    /**
+     * @brief last() - intended to replace RS_Container::lastEntity()
+     * @return - the last entity
+     */
     RS_Entity* last();
+    /**
+     * @brief next() - intended to replace RS_Container::nextEntity()
+     * @return - the next entity
+     */
     RS_Entity* next();
+    /**
+     * @brief prev - the entity traversed before the current one
+     *               This is NOT the same as RS_Container::prevEntity()
+     * @return the previous entity traversed
+     */
     RS_Entity* prev();
     std::vector<RS_Entity*> entities();
 
 
 private:
+    // One traverse by one step
     RS_Entity* get();
+    // Collect entities in the container by the DFS order
     void collect(std::vector<RS_Entity*>& items, const RS_EntityContainer* container) const;
     // for forward/backward support
     size_t currentIndex() const;
     struct Data;
     std::unique_ptr<LC_ContainerTraverser::Data> m_pImp;
     LC_ContainerTraverser::Direction m_direction = Direction::Forward;
-
-
 };
-
 }
-
 #endif // LC_ContainerTraverser_H
 
 
