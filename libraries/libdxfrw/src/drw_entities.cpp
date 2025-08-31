LibreCAD/LibreCAD

commit 944e6340fe5a1e17a4e44d9de25c78131bfb4cb5

Ordinate Dimensions (#2131)
* small fixes for weird build errors for Linux and MacOS

* support of Ordinate dimensions (entities, dxf read/write, actions).
Initial support for dimstyles etc (WIP).

* Revert "small fixes for weird build errors for Linux and MacOS"

This reverts commit 0e50d0047849bea409122bfe3069bd2abce6cc4f.

* small fixes for weird build errors for Linux and MacOS

* small fixes for weird build errors for Linux and MacOS

* redundant virtual modifiers cleanup

* select window - dimensions fix

* code cleanup

* readme update

* dimensions related refactoring and cleanup, fix !!! for RS_DimAligned crash on hover (due to missed detach() in clone()).

* merge cleanup

* initial support of tolerance (WIP)

* merge

* merge

---------

Co-authored-by: sand1024 <sand1024@gmail.com>
Co-authored-by: Dongxu Li <dongxuli2011@gmail.com>

libraries/libdxfrw/src/drw_entities.cpp
@@ -1121,6 +1121,54 @@ bool DRW_Insert::parseDwg(DRW::Version version, dwgBuffer *buf, duint32 bs){
     return buf->isGood();
 }
 
+void DRW_Entity::init(DRW_Entity const& rhs) {
+}
+
+bool DRW_Tolerance::parseCode(int code, dxfReader* reader) {
+    switch (code) {
+        case 10:
+            insertionPoint.x = reader->getDouble();
+            break;
+        case 20:
+            insertionPoint.y = reader->getDouble();
+            break;
+        case 30:
+            insertionPoint.z = reader->getDouble();
+            break;
+        case 11:
+            xAxisDirectionVector.x = reader->getDouble();
+            break;
+        case 21:
+            xAxisDirectionVector.y = reader->getDouble();
+            break;
+        case 31:
+            xAxisDirectionVector.z = reader->getDouble();
+            break;
+        case 210:
+            extPoint.x = reader->getDouble();
+            break;
+        case 220:
+            extPoint.y = reader->getDouble();
+            break;
+        case 230:
+            extPoint.z = reader->getDouble();
+            break;
+        case 1:
+            text = reader->getUtf8String();
+            break;
+        case 3:
+            dimStyleName = reader->getUtf8String();
+            break;
+        default:
+            return DRW_Entity::parseCode(code, reader);
+    }
+}
+
+bool DRW_Tolerance::parseDwg(DRW::Version v, dwgBuffer* buf, duint32 bs) {
+    DRW_DBG("PARSING TOLERANCE FROM DWG IS NOT SUPPORTED!!!");
+    return true;
+}
+
 void DRW_LWPolyline::applyExtrusion(){
     if (haveExtrusion) {
         calculateAxis(extPoint);
@@ -1325,7 +1373,6 @@ bool DRW_Text::parseCode(int code, dxfReader *reader){
     default:
         return DRW_Line::parseCode(code, reader);
     }
-
     return true;
 }
 

commit caddcb0ca9c78dbad0e08689a0923c118a99608d

drw_entities: add missing return in DRW_Tolerance::parseCode()
The below commit added DRW_Tolerance::parseCode() without actually
returning anything from all except the default case. Return `true` in
that case.

The compiler correctly complains:
```
src/drw_entities.cpp:1165:1: error: control reaches end of non-void function [-Werror=return-type]
```

Fixes: 944e6340fe5a (Ordinate Dimensions (#2131))

libraries/libdxfrw/src/drw_entities.cpp
@@ -1162,6 +1162,7 @@ bool DRW_Tolerance::parseCode(int code, dxfReader* reader) {
         default:
             return DRW_Entity::parseCode(code, reader);
     }
+    return true;
 }
 
 bool DRW_Tolerance::parseDwg(DRW::Version v, dwgBuffer* buf, duint32 bs) {