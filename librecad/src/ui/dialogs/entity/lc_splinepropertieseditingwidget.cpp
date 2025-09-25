b6d341565328e77d57cb7ea4c5da04c1056aa9da
dongxuli-concord-design <102729580+dongxuli-concord-design@users.noreply.github.com>
2025-09-24 23:45:43 -0400

    building issue

--- librecad/src/ui/dialogs/entity/lc_splinepropertieseditingwidget.cpp
+++ librecad/src/ui/dialogs/entity/lc_splinepropertieseditingwidget.cpp
@@ -18,6 +18,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+#include <stdexcept>
+
 #include <QAction>
 #include <QMenu>
 #include <QMessageBox>
