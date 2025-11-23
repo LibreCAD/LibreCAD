3588673a0c0a3393e942db1f74a14db773df3e1c
Dongxu Li <dongxuli2011@gmail.com>
2025-11-23 13:53:11 -0500

    copyright

--- librecad/src/lib/engine/document/entities/lc_splinehelper.h
+++ librecad/src/lib/engine/document/entities/lc_splinehelper.h
@@ -1,3 +1,26 @@
+/****************************************************************************
+**
+** This file is part of the LibreCAD project, a 2D CAD program
+**
+** Copyright (C) 2025 librecad.org
+** Copyright (C) 2025 Dongxu Li (github.com/dxli)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**********************************************************************/
+// File: lc_splinehelper.h
+
 #ifndef LC_SPLINEHELPER_H
 #define LC_SPLINEHELPER_H
 
