a5ffe9a3fd57a6fdff3e7de06e107f23d039288e
Dongxu Li <dongxuli2011@gmail.com>
2025-12-12 08:30:41 -0800

    Issue #2403: aarch64 AppImage qt plugin folder fixes (#2404)
    * plugins
    
    * copy plugins
    
    * force qt plugins folder

--- CI/build-appimg-aarch64.sh
+++ CI/build-appimg-aarch64.sh
@@ -43,16 +43,28 @@ strip unix/resources/plugins/*.so
 
 # copy executables and binary resources
 cp unix/librecad appdir/usr/bin/
+
+# <<< FORCE BUNDLED QT PLUGINS >>>
+# Add qt.conf next to the executable to force Qt to load plugins only from the bundled location
+cat > appdir/usr/bin/qt.conf <<EOF
+[Paths]
+Plugins = ../plugins
+EOF
+# <<< END >>>
+
 cp unix/resources/plugins/*.so appdir/usr/lib/librecad/
 cp -r unix/resources/qm appdir/usr/share/librecad/
 
 cp desktop/librecad.desktop appdir/usr/share/applications/
 cp desktop/org.librecad.librecad.appdata.xml appdir/usr/share/metainfo/
-
 cp -r librecad/support/doc/* appdir/usr/share/doc/librecad/
 cp -r librecad/support/fonts appdir/usr/share/librecad/
 cp -r librecad/support/library appdir/usr/share/librecad/
 cp -r librecad/support/patterns appdir/usr/share/librecad/
+mkdir -p appdir/usr/lib/aarch64-linux-gnu/qt5/plugins
+cp -r /usr/lib/aarch64-linux-gnu/qt5/plugins/* appdir/usr/lib/aarch64-linux-gnu/qt5/plugins
+mkdir -p appdir/usr/plugins/
+cp -r /usr/lib/aarch64-linux-gnu/qt5/plugins/* appdir/usr/plugins/
 
 cp CI/librecad.svg appdir/usr/share/icons/hicolor/scalable/apps/
 convert -resize 256x256 CI/librecad.svg appdir/usr/share/icons/hicolor/256x256/apps/librecad.png
@@ -63,7 +75,7 @@ ls -l appdir/usr/share/icons/hicolor/256x256/apps/librecad.png
 ls -l appdir/usr/share/icons/hicolor/scalable/apps/
 ls -l appdir/usr/share/applications/librecad.desktop
 
-export QMAKE=$(which qmake6)
+export QMAKE=$(which qmake)
 export EXTRA_QT_MODULES=svg
 
 wget -c https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous -O - | grep "appimagetool-.*-aarch64.AppImage" | head -n 1 | cut -d '"' -f 2)
@@ -72,6 +84,6 @@ wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/lin
 wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-aarch64.AppImage
 chmod +x *.AppImage
 #ARCH=aarch64 ./appimagetool-*.AppImage deploy appdir/usr/share/applications/librecad.desktop
-ARCH=aarch64 ./linuxdeploy-aarch64.AppImage --appdir appdir -e appdir/usr/bin/librecad -d appdir/usr/share/applications/librecad.desktop
 ARCH=aarch64 ./linuxdeploy-plugin-qt-aarch64.AppImage --appdir appdir
+ARCH=aarch64 ./linuxdeploy-aarch64.AppImage --appdir appdir -e appdir/usr/bin/librecad -d appdir/usr/share/applications/librecad.desktop
 VERSION=`git describe --always` ARCH=aarch64 ./appimagetool-*.AppImage appdir/
