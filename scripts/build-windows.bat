ad80630cb7f2d24933bbd619f983886d3d5a0e2d
dongxuli-concord-design <102729580+dongxuli-concord-design@users.noreply.github.com>
2026-01-02 05:22:06 -0500

    TS fixes

--- scripts/build-windows.bat
+++ scripts/build-windows.bat
@@ -31,11 +31,27 @@ rem Improved windeployqt: verbose, force copy, standard plugin subdirs
 echo windepolyqt: Current directory is: %CD%
 dir windows\
 dir librecad\ts\*.ts
+windeployqt.exe --release windows\LibreCAD.exe --verbose 2 --force
+lrelease.exe librecad\ts\*.ts -qm windows\ts\
 dir windows\ts\*.ts
-windeployqt.exe --release windows\LibreCAD.exe
-#lrelease.exe librecad\ts\*.ts -qm windows\ts\
 rem windeployqt.exe windows\LibreCAD.exe --release --verbose 2 --force
 
 popd
 
+call set-windows-env.bat
+
+if _%LC_NSIS_FILE%==_ (
+    set LC_NSIS_FILE=nsis-msvc.nsi
+)
+
+pushd postprocess-windows
+
+rem Detect 64-bit build environment and pass /DWIN64 if applicable
+set NSIS_FLAGS=/X"SetCompressor /FINAL lzma" /V4
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    set NSIS_FLAGS=%NSIS_FLAGS% /DWIN64
+)
+
+makensis.exe %NSIS_FLAGS% %LC_NSIS_FILE%
+popd
 call build-win-setup.bat
