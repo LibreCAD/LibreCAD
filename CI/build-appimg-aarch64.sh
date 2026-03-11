3eb3a436269f2529e6f4998a29828401235a4e3e
dxli <dongxuli2011@gmail.com>
2026-03-11 01:26:16 -0400

    debug

--- CI/build-appimg-aarch64.sh
+++ CI/build-appimg-aarch64.sh
@@ -54,8 +54,9 @@ EOF
 
 cp unix/resources/plugins/*.so appdir/usr/lib/librecad/
 mkdir appdir/usr/share/librecad/qm
-cp unix/resources/qm/* appdir/usr/share/librecad/qm/
-cp unix/*.qm appdir/usr/share/librecad/qm/
+find unix -type f -iname "*.qm"
+cp -v unix/resources/qm/* appdir/usr/share/librecad/qm/
+cp -v unix/*.qm appdir/usr/share/librecad/qm/
 
 cp desktop/librecad.desktop appdir/usr/share/applications/
 cp desktop/org.librecad.librecad.appdata.xml appdir/usr/share/metainfo/
