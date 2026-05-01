7e0db0f35e00d6b60a780e0c5c9116b9b09cc01d
Dongxu Li <dongxuli2011@gmail.com>
2026-05-01 20:57:55 +0800

    CI: 2.2.1.5 preparation (#2544)
    * CI: cleanup
    
    * gitignore
    
    * CI: snap fixes

--- scripts/createSnap.sh
+++ scripts/createSnap.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+# Local snap build (destructive mode, no LXD/Multipass).
+# CI uses canonical/action-build@v1 instead; see .github/workflows/build-all.yml.
+
 sudo snap install snapcraft --classic
 
 sudo snap run snapcraft --destructive-mode
- 
+
 sudo snap remove librecad --purge
 #sudo snap install --dangerous librecad_2.2.1-rc_amd64.snap --jailmode
-
