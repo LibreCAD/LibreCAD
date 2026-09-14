97b9762607e416638587f8f2671a992a89a13237
Dongxu Li <dongxuli2011@gmail.com>
2026-09-14 14:57:26 -0400

    macOS: place built-in plugins in signed code directory

--- plugins/divide/divide.pro
+++ plugins/divide/divide.pro
@@ -24,7 +24,7 @@ win32 {
 }
 unix {
     macx {
-        DESTDIR = ../../LibreCAD.app/Contents/Resources/plugins
+        DESTDIR = ../../LibreCAD.app/Contents/PlugIns/LibreCAD
     }
     else {
         DESTDIR = ../../unix/resources/plugins
--- scripts/postprocess-osx.sh
+++ scripts/postprocess-osx.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+# ********************************************************************************
+# This file is part of the LibreCAD project, a 2D CAD program
+#
+# Copyright (C) 2011-2026 LibreCAD.org
+# Copyright (C) 2011-2026 Dongxu Li (github.com/dxli)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
+# ********************************************************************************
+
 # sh path/to/script <.app directory for output bundle> <path to qt bin>
 
 CONTENTSDIR="$1/Contents"
 LRELEASE="$2/lrelease"
-SCRIPTDIR="$(dirname $0)"
+SCRIPTDIR="$(dirname "$0")"
 
 RESOURCEDIR="$CONTENTSDIR/Resources"
 TSDIRLC="$SCRIPTDIR/../librecad/ts"
@@ -14,7 +36,7 @@ TSDIRPI="$SCRIPTDIR/../plugins/ts"
 mkdir -p "$RESOURCEDIR/fonts"
 mkdir -p "$RESOURCEDIR/patterns"
 mkdir -p "$RESOURCEDIR/library"
-mkdir -p "$CONTENTSDIR/PlugIns"
+mkdir -p "$CONTENTSDIR/PlugIns/LibreCAD"
 if [ -d "$SCRIPTDIR/../librecad/support/patterns" ]; then
 	find "$SCRIPTDIR/../librecad/support/patterns" -maxdepth 1 -type f -name '*.dxf' \
 		-exec cp {} "$RESOURCEDIR/patterns/" \;
@@ -24,21 +46,21 @@ if [ -d "$SCRIPTDIR/../librecad/support/library" ]; then
 	cp -r "$SCRIPTDIR/../librecad/support/library/" "$RESOURCEDIR/library/"
 fi
 
-if [ -x $LRELEASE ]
+if [ -x "$LRELEASE" ]
 then
 	# Generate translations
-	$LRELEASE "$SCRIPTDIR/../librecad/src/src.pro"
-	$LRELEASE "$SCRIPTDIR/../plugins/plugins.pro"
+	"$LRELEASE" "$SCRIPTDIR/../librecad/src/src.pro"
+	"$LRELEASE" "$SCRIPTDIR/../plugins/plugins.pro"
 	mkdir -p "$RESOURCEDIR/qm"
 
 	for tf in "$TSDIRLC/"*.qm
 	do
-		mv "$tf" "$RESOURCEDIR/qm/$(basename $tf)"
+		mv "$tf" "$RESOURCEDIR/qm/$(basename "$tf")"
 	done
 
 	for tf in "$TSDIRPI/"*.qm
 	do
-		mv "$tf" "$RESOURCEDIR/qm/$(basename $tf)"
+		mv "$tf" "$RESOURCEDIR/qm/$(basename "$tf")"
 	done
 else
 	echo "WARNING: lrelease not found - Translations will not be generated"
